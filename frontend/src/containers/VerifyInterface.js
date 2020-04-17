54ce9edd43e310ea6595eb87be771d3ab9e94bca
Douwe Kiela <douwe.kiela@cl.cam.ac.uk>
2020-04-17 06:05:57 +0000

    Verify interface stub

--- frontend/src/containers/VerifyInterface.js
+++ frontend/src/containers/VerifyInterface.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 class VerifyInterface extends React.Component {
   render() {
-    return <>Create Interface</>;
+    return (
+      <Container>
+        <Row>
+          <h2>Verify examples that fooled the model - {this.state.task.name}</h2>
+        </Row>
+        <Row>
+          <CardGroup style={{marginTop: 20, width: '100%'}}>
+            <Card border='dark'>
+              <Card.Body>
+                The verification interface is still under construction.
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Row>
+      </Container>
+    );
   }
 }
 
