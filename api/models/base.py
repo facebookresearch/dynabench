d9a56274c1394247a0f9b07274eacf31639e5752
Douwe Kiela <douwe.kiela@cl.cam.ac.uk>
2020-04-24 19:38:46 +0000

    Fix error in base model update method

--- api/models/base.py
+++ api/models/base.py
@@ -1,23 +1,28 @@
 import sqlalchemy as db
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 # now this is very ugly..
 def connect_db():
-    engine = db.create_engine('mysql+pymysql://dynabench:dynabench@localhost:3306/dynabench', pool_pre_ping=True)
+    engine = db.create_engine('mysql+pymysql://dynabench:dynabench@localhost:3306/dynabench', pool_pre_ping=True, pool_recycle=3600)
     connection = engine.connect()
     Base.metadata.bind = engine
-    DBSession = sessionmaker(bind=engine)
-    return DBSession()
+    Session = scoped_session( sessionmaker() )
+    return Session
+    #DBSession = sessionmaker(bind=engine)
+    #return DBSession()
 
-dbs = connect_db()
+DBSession = connect_db()
 
 class BaseModel():
     def __init__(self, model):
         self.model = model
-        self.dbs = dbs #connect_db()
+        #self.dbs = dbs #connect_db()
+        self.dbs = DBSession()
+    def __del__(self):
+        self.dbs.close()
 
     def get(self, id):
         try:
@@ -32,4 +37,4 @@ class BaseModel():
     def update(self, id, kwargs):
         t = self.dbs.query(self.model).filter(self.model.id == id)
         t.update(kwargs)
-        dbs.commit()
+        self.dbs.commit()
