94204e3c5eeffd96563d29d82e96e4f8d15065cc
Douwe Kiela <douwe.kiela@cl.cam.ac.uk>
2020-06-23 05:26:16 +0000

    Add NLI Mturk task

---
+++ frontends/mturk/src/components/nli-1/core.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { CreateInterface } from '../CreateInterface.js';
+
+class NLITaskPreview extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <>
+        <h1>Adversarial Natural Language Inference</h1>
+        <p>In this task, you will be asked to find examples that fool an AI model into making the wrong prediction.</p>
+      </>;
+  }
+}
+
+class NLITaskOnboarder extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <>
+      <h1>Onboarding</h1>
+      <p>Task onboarding</p>
+      </>;
+  }
+}
+
+class NLITaskMain extends React.Component {
+  constructor(props) {
+    super(props);
+    this.api = props.api;
+  }
+  render() {
+    return <CreateInterface api={this.api} />;
+  }
+}
+
+export { NLITaskPreview, NLITaskOnboarder, NLITaskMain };
