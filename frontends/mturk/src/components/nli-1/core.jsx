c38d9ae4add350c7d558d8f14d077e885fdaa271
Douwe Kiela <douwe.kiela@cl.cam.ac.uk>
2020-06-24 03:19:05 +0000

    Allow for specifying the desired task id from the Mturk deployment script

--- frontends/mturk/src/components/nli-1/core.jsx
+++ frontends/mturk/src/components/nli-1/core.jsx
@@ -30,6 +30,7 @@ class NLITaskMain extends React.Component {
   constructor(props) {
     super(props);
     this.api = props.api;
+    console.log(props);
   }
   render() {
     return <CreateInterface api={this.api} />;
