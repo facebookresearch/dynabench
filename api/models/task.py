5adea0301a1a74fee65755c8c2ca611f404084ce
Divyansh Kaushik <divyanshk@fb.com>
2020-06-24 14:15:58 -0400

    Adding hidden tasks to /tasks page

--- api/models/task.py
+++ api/models/task.py
@@ -2,9 +2,11 @@ import sqlalchemy as db
 from .base import Base, BaseModel
 from .round import Round
 
+
 class Task(Base):
     __tablename__ = 'tasks'
-    __table_args__ = { 'mysql_charset': 'utf8mb4', 'mysql_collate': 'utf8mb4_general_ci' }
+    __table_args__ = {'mysql_charset': 'utf8mb4',
+                      'mysql_collate': 'utf8mb4_general_ci'}
 
     id = db.Column(db.Integer, primary_key=True)
 
@@ -13,11 +15,13 @@ class Task(Base):
 
     desc = db.Column(db.String(length=255))
     longdesc = db.Column(db.Text)
-    targets = db.Column(db.Text) # ordered list of target labels
-    score_progression = db.Column(db.Text) # ordered list of max scores per round
+    targets = db.Column(db.Text)  # ordered list of target labels
+    # ordered list of max scores per round
+    score_progression = db.Column(db.Text)
 
     total_verified = db.Column(db.Integer, default=0)
     total_collected = db.Column(db.Integer, default=0)
+    hidden = db.Column(db.Boolean, default=False)
     last_updated = db.Column(db.DateTime)
 
     #cur_round = db.Column(db.Integer, db.ForeignKey("rounds.id"), nullable=False)
@@ -34,26 +38,31 @@ class Task(Base):
             d[column.name] = str(getattr(self, column.name))
         return d
 
+
 class TaskModel(BaseModel):
     def __init__(self):
         super(TaskModel, self).__init__(Task)
+
     def getByShortName(self, shortname):
         try:
             return self.dbs.query(Task).filter(Task.shortname == shortname).one()
         except db.orm.exc.NoResultFound:
             return False
-    def listWithRounds(self):
-        rows = self.dbs.query(Task, Round).join(Round, (Round.tid == Task.id) & (Round.rid == Task.cur_round)).all()
+
+    def listWithRounds(self, exclude_hidden=True):
+        rows = self.dbs.query(Task, Round).join(
+            Round, (Round.tid == Task.id) & (Round.rid == Task.cur_round)).all()
         tasks = [x[0].to_dict() for x in rows]
         for ii, r in enumerate([x[1] for x in rows]):
             tasks[ii]['round'] = r.to_dict()
         return tasks
+
     def getWithRound(self, tid):
         try:
-            t, r = self.dbs.query(Task, Round).filter(Task.id == tid).join(Round, (Round.tid == Task.id) & (Round.rid == Task.cur_round)).one()
+            t, r = self.dbs.query(Task, Round).filter(Task.id == tid).join(
+                Round, (Round.tid == Task.id) & (Round.rid == Task.cur_round)).one()
             t = t.to_dict()
             t['round'] = r.to_dict()
             return t
         except db.orm.exc.NoResultFound:
             return False
-
--- frontends/web/src/containers/TasksPage.js
+++ frontends/web/src/containers/TasksPage.js
@@ -1,67 +1,94 @@
-import React from 'react';
-import {
-  Container,
-  Row,
-  Card,
-  CardGroup,
-  Table
-} from 'react-bootstrap';
-import Moment from 'react-moment';
+import React from "react";
+import { Container, Row, Card, CardGroup, Table } from "react-bootstrap";
+import Moment from "react-moment";
 
-import UserContext from './UserContext';
+import UserContext from "./UserContext";
 
 class TasksPage extends React.Component {
   static contextType = UserContext;
   constructor(props) {
     super(props);
     this.state = {
-      tasks: []
-    }
+      tasks: [],
+    };
   }
   componentDidMount() {
-    this.context.api.getTasks()
-    .then(result => {
-      this.setState({tasks: result})
-    })
-    .catch(error => {
-      console.log(error);
-    });
+    this.context.api
+      .getTasks()
+      .then((result) => {
+        this.setState({ tasks: result });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
   render() {
-    const taskCards = this.state.tasks.map((task, index) =>
-      <CardGroup key={task.id} style={{marginTop: 20, width: '100%'}}>
-        <Card className="taskCard" onClick={() => this.props.history.push("/tasks/" + task.id)}>
-          <span className={index < 3 ? "taskHeader taskHeaderRight" : "taskHeader taskHeaderLeft"}>
-            <h2 className="text-uppercase">{task.name} ({task.shortname})</h2>
-          </span>
-          <Card.Body>
-            <Card.Text>
-              {task.desc}
-            </Card.Text>
-            <Table className="table-striped">
-            <thead></thead>
-            <tbody>
-            <tr><td>Round:</td><td>{task.cur_round}</td></tr>
-          <tr><td>Verified/Collected<br/>(model error rate):</td><td>{task.round.total_verified}/{task.round.total_collected}<br/>({task.round.total_collected > 0 ? (task.round.total_verified / task.round.total_collected).toFixed(2) : '0.00'}%)</td></tr>
-            </tbody>
-            </Table>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">
-             Last updated  <Moment utc fromNow>{task.last_updated}</Moment>
-            </small>
-          </Card.Footer>
-        </Card>
+    const taskCards = this.state.tasks.map((task, index) => (
+      <CardGroup key={task.id} style={{ marginTop: 20, width: "100%" }}>
+        {task.hidden === "False" ? (
+          <Card
+            className="taskCard"
+            onClick={() => this.props.history.push("/tasks/" + task.id)}
+          >
+            <span
+              className={
+                index < 3
+                  ? "taskHeader taskHeaderRight"
+                  : "taskHeader taskHeaderLeft"
+              }
+            >
+              <h2 className="text-uppercase">
+                {task.name} ({task.shortname})
+              </h2>
+            </span>
+            <Card.Body>
+              <Card.Text>{task.desc}</Card.Text>
+              <Table className="table-striped">
+                <thead></thead>
+                <tbody>
+                  <tr>
+                    <td>Round:</td>
+                    <td>{task.cur_round}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Verified/Collected
+                      <br />
+                      (model error rate):
+                    </td>
+                    <td>
+                      {task.round.total_verified}/{task.round.total_collected}
+                      <br />(
+                      {task.round.total_collected > 0
+                        ? (
+                          task.round.total_verified /
+                          task.round.total_collected
+                        ).toFixed(2)
+                        : "0.00"}
+                      %)
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">
+                Last updated{" "}
+                <Moment utc fromNow>
+                  {task.last_updated}
+                </Moment>
+              </small>
+            </Card.Footer>
+          </Card>
+        ) : (null)}
       </CardGroup>
-    );
+    ));
     return (
       <Container>
         <Row>
           <h2>TASKS</h2>
         </Row>
-        <Row>
-          {taskCards}
-        </Row>
+        <Row>{taskCards}</Row>
       </Container>
     );
   }
