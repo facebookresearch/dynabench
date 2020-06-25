2ed730c90d7d286b5bb56c41c4b5e0a745ff8387
aswin-ideas2it <aswin@ideas2it.com>
2020-06-25 16:21:03 +0530

    Package update & loading of tasks through context api

--- frontends/web/src/containers/App.js
+++ frontends/web/src/containers/App.js
@@ -23,6 +23,7 @@ import ContactPage from "./ContactPage";
 import TermsPage from "./TermsPage";
 import DataPolicyPage from "./DataPolicyPage";
 import UserContext from "./UserContext";
+import TasksContext from "./TasksContext";
 import UserPage from "./UserPage";
 import ModelPage from "./ModelPage";
 import ApiService from "./ApiService";
@@ -35,6 +36,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       user: {},
+      tasks: [],
     };
     this.updateState = this.updateState.bind(this);
     this.api = new ApiService();
@@ -46,6 +48,14 @@ class App extends React.Component {
     if (this.api.loggedIn()) {
       this.setState({ user: this.api.getCredentials() });
     }
+    this.api
+      .getTasks()
+      .then((result) => {
+        this.setState({ tasks: result });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     // else {
     //  var token = this.api.getToken();
     //  if (token) {
@@ -54,6 +64,15 @@ class App extends React.Component {
     //}
   }
   render() {
+    const NavItems = this.state.tasks.map((task, index) => (
+      <NavDropdown.Item
+        key={task.id}
+        href={`/tasks/${task.id}`}
+        className="py-3"
+      >
+        {task.name}
+      </NavDropdown.Item>
+    ));
     return (
       <UserContext.Provider
         value={{
@@ -62,119 +81,127 @@ class App extends React.Component {
           api: this.api,
         }}
       >
-      <Router>
-        <ScrollToTop />
-          <Navbar
-            expand="lg"
-            variant="dark"
-            className="shadow blue-bg justify-content-start"
-          >
-          <Navbar.Toggle aria-controls="basic-navbar-nav" className="border-0 mr-2" />
-            <Navbar.Brand as={Link} to="/">
-              <img
-                src="/logo_w.png"
-                style={{ width: 80, marginLeft: 5, marginRight: 20 }}
-                alt="DynaBench"
+        <TasksContext.Provider
+          value={{
+            tasks: this.state.tasks,
+          }}
+        >
+          <Router>
+            <ScrollToTop />
+            <Navbar
+              expand="lg"
+              variant="dark"
+              className="shadow blue-bg justify-content-start"
+            >
+              <Navbar.Toggle
+                aria-controls="basic-navbar-nav"
+                className="border-0 mr-2"
               />
-            </Navbar.Brand>
-          <Navbar.Collapse>
-          <Nav className="mr-auto">
-            <Nav.Item>
-                <Nav.Link as={Link} to="/about">
-                  About
-                </Nav.Link>
-            </Nav.Item>
-              <NavDropdown title="Tasks" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/tasks/1" className="gray-color py-3">
-                  Natural Language Inference
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/tasks/2" className="gray-color py-3">
-                  Question Answering
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/tasks/3" className="gray-color py-3">
-                  Sentiment Analysis
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/tasks/5" className="gray-color py-3">
-                  Hate Speech
-                </NavDropdown.Item>
-              </NavDropdown>
-          </Nav>
-          <Nav className="justify-content-end">
-              {this.state.user.id ? (
-                <>
-                <Nav.Item>
-                    <Nav.Link as={Link} to={"/profile"}>
-                      {this.state.user.username}
+              <Navbar.Brand as={Link} to="/">
+                <img
+                  src="/logo_w.png"
+                  style={{ width: 80, marginLeft: 5, marginRight: 20 }}
+                  alt="DynaBench"
+                />
+              </Navbar.Brand>
+              <Navbar.Collapse>
+                <Nav className="mr-auto">
+                  <Nav.Item>
+                    <Nav.Link as={Link} to="/about">
+                      About
                     </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link as={Link} to={"/logout"}>
-                      Logout
-                    </Nav.Link>
-                </Nav.Item>
-                </>
-              ) : (
-                <>
-                <Nav.Item>
-                    <Nav.Link as={Link} to="/login">
-                      Login
-                    </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link as={Link} to="/register" className="signup-nav-link">
-                      Sign up
-                    </Nav.Link>
-                </Nav.Item>
-                </>
-              )}
-          </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <div id="content">
-        <Switch>
-          <Route path="/about" component={AboutPage} />
-          <Route path="/contact" component={ContactPage} />
-          <Route path="/termsofuse" component={TermsPage} />
-          <Route path="/datapolicy" component={DataPolicyPage} />
-          <Route path="/tasks/:taskId/create" component={CreateInterface} />
-          <Route path="/tasks/:taskId/verify" component={VerifyInterface} />
-          <Route path="/tasks/:taskId" component={TaskPage} />
-          <Route path="/tasks" component={TasksPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/forgot" component={ForgotPassword} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/profile" component={ProfilePage} />
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/users/:userId" component={UserPage} />
-          <Route path="/models/:modelId" component={ModelPage} />
-          <Route path="/" component={HomePage} />
-        </Switch>
-        </div>
-        <footer className="footer text-white">
-            <Container fluid>
-            <Row>
-                <div className="footer-nav-link">
-                  Copyright © 2020 Facebook, Inc.
-                </div>
-                <div className="footer-nav-link">
-                  <Link to="/contact" className="text-reset">
-                    Contact
-                  </Link>
-                </div>
-                <div className="footer-nav-link">
-                  <Link to="/termsofuse" className="text-reset">
-                    Terms of Use
-                  </Link>
-                </div>
-                <div className="footer-nav-link">
-                  <Link to="/datapolicy" className="text-reset">
-                    Data Policy
-                  </Link>
-                </div>
-            </Row>
-          </Container>
-        </footer>
-      </Router>
+                  </Nav.Item>
+                  <NavDropdown title="Tasks" id="basic-nav-dropdown">
+                    {NavItems}
+                  </NavDropdown>
+                </Nav>
+                <Nav className="justify-content-end">
+                  {this.state.user.id ? (
+                    <>
+                      <Nav.Item>
+                        <Nav.Link as={Link} to={"/profile"}>
+                          {this.state.user.username}
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link as={Link} to={"/logout"}>
+                          Logout
+                        </Nav.Link>
+                      </Nav.Item>
+                    </>
+                  ) : (
+                    <>
+                      <Nav.Item>
+                        <Nav.Link as={Link} to="/login">
+                          Login
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link
+                          as={Link}
+                          to="/register"
+                          className="signup-nav-link"
+                        >
+                          Sign up
+                        </Nav.Link>
+                      </Nav.Item>
+                    </>
+                  )}
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <div id="content">
+              <Switch>
+                <Route path="/about" component={AboutPage} />
+                <Route path="/contact" component={ContactPage} />
+                <Route path="/termsofuse" component={TermsPage} />
+                <Route path="/datapolicy" component={DataPolicyPage} />
+                <Route
+                  path="/tasks/:taskId/create"
+                  component={CreateInterface}
+                />
+                <Route
+                  path="/tasks/:taskId/verify"
+                  component={VerifyInterface}
+                />
+                <Route path="/tasks/:taskId" component={TaskPage} />
+                <Route path="/tasks" component={TasksPage} />
+                <Route path="/login" component={LoginPage} />
+                <Route path="/forgot" component={ForgotPassword} />
+                <Route path="/logout" component={Logout} />
+                <Route path="/profile" component={ProfilePage} />
+                <Route path="/register" component={RegisterPage} />
+                <Route path="/users/:userId" component={UserPage} />
+                <Route path="/models/:modelId" component={ModelPage} />
+                <Route path="/" component={HomePage} />
+              </Switch>
+            </div>
+            <footer className="footer text-white">
+              <Container fluid>
+                <Row>
+                  <div className="footer-nav-link">
+                    Copyright © 2020 Facebook, Inc.
+                  </div>
+                  <div className="footer-nav-link">
+                    <Link to="/contact" className="text-reset">
+                      Contact
+                    </Link>
+                  </div>
+                  <div className="footer-nav-link">
+                    <Link to="/termsofuse" className="text-reset">
+                      Terms of Use
+                    </Link>
+                  </div>
+                  <div className="footer-nav-link">
+                    <Link to="/datapolicy" className="text-reset">
+                      Data Policy
+                    </Link>
+                  </div>
+                </Row>
+              </Container>
+            </footer>
+          </Router>
+        </TasksContext.Provider>
       </UserContext.Provider>
     );
   }
---
+++ frontends/web/src/containers/TasksContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TasksContext = React.createContext({tasks: []});
+
+export default TasksContext;
