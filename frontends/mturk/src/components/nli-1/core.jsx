87805cacc2901f6fd7d6e5f92380e58287432389
Douwe Kiela <douwe.kiela@cl.cam.ac.uk>
2020-06-25 00:58:37 +0000

    Make create interface work properly in Mturk sandbox

--- frontends/mturk/src/components/nli-1/core.jsx
+++ frontends/mturk/src/components/nli-1/core.jsx
@@ -33,7 +33,7 @@ class NLITaskMain extends React.Component {
     console.log(props);
   }
   render() {
-    return <CreateInterface api={this.api} />;
+    return <CreateInterface api={this.api} {...this.props} />;
   }
 }
 
