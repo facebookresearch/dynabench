301b4879158c5c4f5f6f451218d84c2c8b185b48
aswin-ideas2it <aswin@ideas2it.com>
2020-06-25 12:14:13 +0530

    Design changes and added forgotpassword method in API service

--- frontends/web/src/containers/ForgotPassword.js
+++ frontends/web/src/containers/ForgotPassword.js
@@ -1,84 +1,102 @@
-import React from 'react';
-import { Formik } from 'formik';
-import { Container, Row, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import './LoginPage.css';
-import UserContext from './UserContext';
-
-import qs from 'qs';
+import React from "react";
+import { Formik } from "formik";
+import { Container, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./LoginPage.css";
+import UserContext from "./UserContext";
 
 class ForgotPassword extends React.Component {
-  construct() {
-  }
+  construct() {}
   render() {
-    var query = qs.parse(this.props.location.search, { ignoreQueryPrefix: true })
     return (
       <UserContext.Consumer>
-      {props => (
+        {(props) => (
           <Container>
             <Row>
-              <div className="wrapper fadeInDown">
-              <div id="formContent" className="loginForm">
-                <h2>Forgot Password</h2>
-                <p className="msg">{query.msg}</p>
-                <Formik
-                  initialValues={{ email: '' }}
-                  validate={values => {
-                    const errors = {};
-                    if (!values.email) {
-                      errors.email = 'Required';
-                    }
-                    return errors;
-                  }}
-                  onSubmit={(values, { setFieldError, setSubmitting }) => {
-                    props.api.login(values.email, values.password)
-                    .then(result => {
-                      console.log(result);
-                      props.updateState({user: props.api.getCredentials()});
-                      this.props.history.push(values.src);
-                    })
-                    .catch(error => {
-                      console.log(error);
-                      this.setState({error});
-                      setSubmitting(false);
-                      setFieldError('general', 'Authentication failed');
-                    });
-                  }}
-                >
-                  {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting,
-                  }) => (
-                    <>
-                    <form onSubmit={handleSubmit}>
-                      <input
-                        type="email"
-                        name="email"
-                        className="fadeIn first text-left"
-                        placeholder="Email"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.email}
-                      />
-                      <small className="form-text text-muted">{errors.email && touched.email && errors.email}</small>
-                      <small className="form-text text-muted">{errors.general}</small>
-                      <Button type="submit" variant="primary" type="submit" className="btn btn-primary fadeIn third submitBtn button-ellipse" disabled={isSubmitting} >
-                        Send reset link
-                      </Button>
-                    </form>
-                    <div id="formFooter">
-                      <p>Don't have an account? <Link className="btn-link underlineHover" to="/register">Sign up</Link></p>
-                      <p><Link className="btn-link underlineHover" to="/login">Login</Link></p>
-                    </div>
-                    </>
-                  )}
-                </Formik>
-              </div>
+              <div className="wrapper fade-in-down">
+                <div id="formContent" className="login-form text-center">
+                  <h2>Forgot Password</h2>
+                  <Formik
+                    initialValues={{
+                      email: "",
+                    }}
+                    validate={(values) => {
+                      const errors = {};
+                      if (!values.email) {
+                        errors.email = "Required";
+                      }
+                      return errors;
+                    }}
+                    onSubmit={(values, { setFieldError, setSubmitting }) => {
+                      props.api
+                        .forgotPassword(values.email)
+                        .then((result) => {
+                          console.log(result);
+                        })
+                        .catch((error) => {
+                          console.log(error);
+                          this.setState({ error });
+                          setSubmitting(false);
+                        });
+                    }}
+                  >
+                    {({
+                      values,
+                      errors,
+                      touched,
+                      handleChange,
+                      handleBlur,
+                      handleSubmit,
+                      isSubmitting,
+                    }) => (
+                      <>
+                        <form onSubmit={handleSubmit}>
+                          <input
+                            type="email"
+                            name="email"
+                            className="fade-in first text-left"
+                            placeholder="Email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                          />
+                          <small className="form-text text-muted">
+                            {errors.email && touched.email && errors.email}
+                          </small>
+                          <Button
+                            type="submit"
+                            variant="primary"
+                            type="submit"
+                            className="fade-in third submit-btn button-ellipse"
+                            disabled={isSubmitting}
+                          >
+                            SEND RESET LINK
+                          </Button>
+                        </form>
+                        <div className="form-footer">
+                          <p>
+                            Don't have an account?{" "}
+                            <Link
+                              className="btn-link underline-hover"
+                              to="/register"
+                            >
+                              Sign up
+                            </Link>
+                          </p>
+                          <p>
+                            Remember your password?{" "}
+                            <Link
+                              className="btn-link underline-hover"
+                              to="/login"
+                            >
+                              Login
+                            </Link>
+                          </p>
+                        </div>
+                      </>
+                    )}
+                  </Formik>
+                </div>
               </div>
             </Row>
           </Container>
