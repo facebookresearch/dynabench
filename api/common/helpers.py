5574cd5589c3a383d92aff92d55ba67377014686
Ramachandran K <ramachandran@ideas2it.com>
2020-07-02 20:43:01 +0530

    Feature: 	Fetch user models and update profile

--- api/common/helpers.py
+++ api/common/helpers.py
@@ -1,5 +1,10 @@
 import bottle
 
+import common.auth as _auth
+from models.user import UserModel
+
+import logging
+
 def check_fields(data, fields):
     if not data:
         return False
@@ -7,3 +12,50 @@ def check_fields(data, fields):
         if f not in data:
             return False
     return True
+
+def is_current_user(uid, credentials=None):
+    """
+    Validate the user id is currently logged in one.
+    :param uid: User id
+    :param credentials: Authorization detail
+    :return: Boolean
+    """
+    try:
+        if not credentials:
+            token = _auth.jwt_token_from_header()
+            credentials = _auth.get_payload(token)
+        u = UserModel()
+        user = u.get(uid)
+        if not user:
+            return False
+        if uid != credentials['id']:
+            return False
+        return True
+    except Exception as ex:
+        logging.exception('Current user  verification failed  for (%s) exception : %s ' %(uid, ex))
+        return False
+
+def get_limit_and_offset_from_request():
+    """
+    Extract the limit and offset value from request
+    which is help to design the pagination
+    :return: limit, offset
+    """
+
+    try:
+        limit = bottle.request.query.get('limit')
+        offset = bottle.request.query.get('offset')
+        if not limit:
+            limit = 5
+        if not offset:
+            offset = 0
+        # handle if limit in string like ss
+        limit = int(limit)
+        offset = int(offset)
+    except Exception as ex:
+        logging.exception('Query param parsing issue :(%s)' %(ex))
+        limit = 5
+        offset = 0
+
+    return limit, offset
+
--- api/controllers/users.py
+++ api/controllers/users.py
@@ -1,8 +1,10 @@
 import bottle
 
 import common.auth as _auth
+import common.helpers as util
 
 from models.user import UserModel
+from models.model import ModelModel
 
 import json
 
@@ -63,3 +65,76 @@ def create_user():
     token = _auth.get_token({'id': user.id, 'username': user.username})
     logging.info('Registration and authentication successful for %s' % (user.username))
     return {'user': user.to_dict(), 'token': token}
+
+@bottle.get('/users/<uid:int>/models')
+def get_user_models(uid):
+    """
+    Fetch all user models based on user id
+    :param uid:
+    :return: Json Object
+    """
+    # check the current user and request user id are same
+    is_current_user = util.is_current_user(uid=uid)
+    logging.info('Current user validation status (%s) for %s' %(is_current_user, uid))
+    limit, offset = util.get_limit_and_offset_from_request()
+    try:
+        model = ModelModel()
+        results = model.getUserModelsByUid(uid=uid, is_current_user=is_current_user, n=limit, offset=offset)
+        dicts = [model_obj.to_dict() for model_obj in results]
+        if dicts:
+            return json.dumps(dicts)
+        return []
+    except Exception as e:
+        logging.exception('Could not fetch user model(s) : %s' % (e))
+        bottle.abort(400, 'Could not fetch user model(s)')
+
+@bottle.get('/users/<uid:int>/models/<model_id:int>')
+def get_user_models(uid, model_id):
+    """
+    Get users specific model detail
+    :param uid: User Id
+    :param model_id: Model Id
+    :return: Json Object
+    """
+    # check the current user and request user id are same
+    is_current_user = util.is_current_user(uid= uid)
+    logging.info('Current user validation status (%s) for %s' %(is_current_user, uid))
+    try:
+        model = ModelModel()
+        model_obj = model.getUserModelsByUidAndMid(uid=uid, mid=model_id, is_current_user=is_current_user)
+        dicts = model_obj.to_dict()
+        if dicts:
+            return json.dumps(dicts)
+    except Exception as e:
+        logging.exception('Could not fetch user model: %s' % (e))
+        bottle.abort(400, 'Could not fetch user model')
+
+    bottle.abort(204, 'No models found')
+
+@bottle.put('/users/<id:int>/profileUpdate')
+@_auth.requires_auth
+def update_user_profile(credentials, id):
+    """
+    Update user profile details like  real name, affiliation  and user name
+    :param credentials: Authentication detail
+    :param id: User id
+    :return: Json Object
+    """
+    data = bottle.request.json
+    user = UserModel()
+    if not util.check_fields(data, ['username', 'affiliation', 'realname']):
+        bottle.abort(400, 'Missing data')
+
+    # validate user detail
+    if not util.is_current_user(uid=id, credentials=credentials):
+        bottle.abort(403, 'Not authorized to update profile')
+    try:
+        u = user.get(id)
+        user.update(u.id, {'username': data['username'], 'affiliation': data['affiliation'],
+                        'realname': data['realname']})
+        return json.dumps(u.to_dict())
+    except db.orm.exc.NoResultFound as ex:
+        bottle.abort(404, 'User Not found')
+    except Exception as ex:
+        logging.exception('Could not update profile: %s' % (ex))
+        bottle.abort(400, 'Could not update profile: %s' % (ex))
