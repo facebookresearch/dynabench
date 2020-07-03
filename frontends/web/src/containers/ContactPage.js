e6243f80d4805a6ecec767839e8f8090dabba766
aswin-ideas2it <aswin@ideas2it.com>
2020-07-03 09:43:08 +0530

    Code Prettyfy with proper alignments for code quality

--- frontends/web/src/containers/ContactPage.js
+++ frontends/web/src/containers/ContactPage.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import {
-  Container,
-  Col,
-} from 'react-bootstrap';
+import React from "react";
+import { Container, Col } from "react-bootstrap";
 
 const ContactPage = () => (
   <Container>
     <Col className="m-auto" lg={9}>
       <h1 className="my-4 pt-3 text-uppercase">Contact</h1>
-      <p>Please contact <a href="mailto:dynabench@fb.com">dynabench@fb.com</a>.</p>
+      <p>
+        Please contact <a href="mailto:dynabench@fb.com">dynabench@fb.com</a>.
+      </p>
     </Col>
   </Container>
 );
