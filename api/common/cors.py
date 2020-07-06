c5f26a97eca2f44228b44800b82a75ed83a83da4
Douwe Kiela <douwe.kiela@cl.cam.ac.uk>
2020-07-06 18:49:50 +0000

    Handle vary origin in CORS correctly

--- api/common/cors.py
+++ api/common/cors.py
@@ -35,6 +35,7 @@ def add_cors_headers():
     bottle.response.headers['Access-Control-Allow-Headers'] = \
         'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token, Authorization'
     bottle.response.headers['Access-Control-Allow-Credentials'] = 'true'
+    bottle.response.headers['Vary'] = 'Origin'
 @bottle.hook('after_request')
 def add_safety_headers():
     bottle.response.headers['X-Frame-Options'] = 'deny'
