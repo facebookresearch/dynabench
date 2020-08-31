6afa3f55b617995526b5c30d0628d320ee926e9c
Divyansh Kaushik <divyanshkaushik96@gmail.com>
2020-08-31 19:11:29 +0000

    To clean Mephisto servers if task abruptly ended

---
+++ annotators/clean_servers.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('./Mephisto')
+from  mephisto.core.local_database import LocalMephistoDB
+db = LocalMephistoDB()
+requester = db.find_requesters(requester_name="NoahTurk1032_sandbox")[0]
+client = requester._get_client(requester._requester_name)
+from mephisto.providers.mturk.mturk_utils import get_outstanding_hits, expire_and_dispose_hits
+outstanding_hits = get_outstanding_hits(client)
+for hit_type in outstanding_hits:
+    expire_and_dispose_hits(client, outstanding_hits[hit_type])
