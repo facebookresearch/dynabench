6851ed2690b9f69c8885f372e1ef30acdf19e305
Maximilian Bartolo <maxbartolo@fb.com>
2020-09-17 20:15:11 +0100

    Updated maximum n_steps to speed up insights

--- torchserve/tasks/qa/r2/handler.py
+++ torchserve/tasks/qa/r2/handler.py
@@ -127,7 +127,7 @@ class TransformersQAHandler(BaseHandler):
         input_ids, ref_input_ids, attention_mask = construct_input_ref_pair(\
                     example[0]["question"], example[0]["passage"], self.tokenizer, self.device)
         all_tokens = get_word_token(input_ids, self.tokenizer)
-        n_steps = get_n_steps_for_interpretability(len(all_tokens))
+        n_steps = get_n_steps_for_interpretability(n_tokens=len(all_tokens), max_n_steps=8)
         logger.info("Word Tokens = '%s'", all_tokens)
 
         position_qa_model = 0
