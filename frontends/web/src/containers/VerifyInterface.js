6c789546f673ab03478456b15b6d29db3f514ff1
TristanThrush <tristant@mit.edu>
2020-09-21 16:42:16 -0700

    Bugfix/badge overlay for validate (#86)
    Added win badge overlay for validation

--- frontends/web/src/containers/VerifyInterface.js
+++ frontends/web/src/containers/VerifyInterface.js
@@ -10,10 +10,12 @@ import {
   Row,
   Col,
   Card,
-  InputGroup,
-  Button
 } from "react-bootstrap";
 import UserContext from "./UserContext";
+import {
+  OverlayProvider,
+  BadgeOverlay
+} from "./Overlay"
 
 class VerifyInterface extends React.Component {
   static contextType = UserContext;
@@ -91,6 +93,9 @@ class VerifyInterface extends React.Component {
         .validateExample(this.state.example.id, action_label)
         .then((result) => {
           this.getNewExample();
+          if (!!result.badges) {
+            this.setState({showBadges: result.badges})
+          }
         }, (error) => {
           console.log(error);
         });
@@ -98,115 +103,123 @@ class VerifyInterface extends React.Component {
   }
   render() {
     return (
-      <Container className="mb-5 pb-5">
-        <Col className="m-auto" lg={12}>
-          <div className="mt-4 mb-5 pt-3">
-            <p className="text-uppercase mb-0 spaced-header">{this.props.taskName}</p>
-            <h2 className="task-page-header d-block ml-0 mt-0 text-reset">
-              Validate examples
-            </h2>
-            <p>
-              If a model was fooled, we need to make sure that the example is correct.
-            </p>
-          </div>
-          {this.state.task.shortname === "Hate Speech" ?
-            <p className="mt-3 p-3 light-red-bg rounded white-color"><strong>WARNING</strong>: This is sensitive content! If you do not want to see any hateful examples, please switch to another task.</p>
-            : null
-          }
-          <Card className="profile-card overflow-hidden">
-            <div className="mb-1 p-3 light-gray-bg">
-              <h6 className="text-uppercase dark-blue-color spaced-header">Context:</h6>
-              {this.state.example.context && this.state.example.context.context.replace("<br>", "\n")}
+      <OverlayProvider initiallyHide={true}>
+        <BadgeOverlay
+          badgeTypes={this.state.showBadges}
+          show={!!this.state.showBadges}
+          onHide={() => this.setState({showBadges: ""})}
+        >
+        </BadgeOverlay>
+        <Container className="mb-5 pb-5">
+          <Col className="m-auto" lg={12}>
+            <div className="mt-4 mb-5 pt-3">
+              <p className="text-uppercase mb-0 spaced-header">{this.props.taskName}</p>
+              <h2 className="task-page-header d-block ml-0 mt-0 text-reset">
+                Validate examples
+              </h2>
+              <p>
+                If a model was fooled, we need to make sure that the example is correct.
+              </p>
             </div>
-            <Card.Body className="overflow-auto pt-2" style={{ height: 400 }}>
-              <Card
-                className="hypothesis rounded border m-3 card"
-                style={{ minHeight: 120 }}
-              >
-              {this.state.example ?
-                <>
-                <Card.Body className="p-3">
-                  <Row>
-                    <Col xs={12} md={7}>
-                      {this.state.task.type == "extract" ?
-                        <div className="mb-3">
-                          <h6 className="text-uppercase dark-blue-color spaced-header">
-                          Question:
-                          </h6>
-                          <p>
-                          {this.state.example.text}
-                          </p>
-                          <h6 className="text-uppercase dark-blue-color spaced-header">
-                          Answer:
-                          </h6>
-                          <p>
-                          {this.state.example.target_pred}
-                          </p>
-                        </div>
-                        :
-                        <div className="mb-3">
-                          <h6 className="text-uppercase dark-blue-color spaced-header">
-                          {this.state.task.shortname === "NLI" ? "Hypothesis" : "Statement"}:
-                          </h6>
-                          <p>
-                          {this.state.example.text}
-                          </p>
-                          <h6 className="text-uppercase dark-blue-color spaced-header">
-                          Label:
-                          </h6>
-                          <p>
-                          {this.state.example.target}
-                          </p>
-                        </div>
-                      }
-                    </Col>
-                  </Row>
-                </Card.Body>
-                <Card.Footer>
-                  <button
-                    data-index={this.props.index}
-                    onClick={() => this.handleResponse("correct")}
-                    type="button"
-                    className="btn btn-light btn-sm">
-                      <i className="fas fa-thumbs-up"></i> Correct
-                  </button>{" "}
-                  <button
-                    data-index={this.props.index}
-                    onClick={() => this.handleResponse("incorrect")}
-                    type="button"
-                    className="btn btn-light btn-sm">
-                      <i className="fas fa-thumbs-down"></i> Incorrect
-                  </button>{" "}
-                  <button
-                    data-index={this.props.index}
-                    onClick={() => this.handleResponse("flag")}
-                    type="button"
-                    className="btn btn-light btn-sm">
-                      <i className="fas fa-flag"></i> Flag
-                  </button>{" "}
-                  <button
-                    data-index={this.props.index}
-                    onClick={this.getNewExample}
-                    type="button"
-                    className="btn btn-light btn-sm pull-right">
-                      <i className="fas fa-undo-alt"></i> Skip and load new example
-                  </button>
-                </Card.Footer>
-                </>
-                :
-                <Card.Body className="p-3">
-                  <Row>
-                    <Col xs={12} md={7}>
-                      <p>No more examples to be verified. Please create more examples!</p>
-                    </Col>
-                  </Row>
-                </Card.Body>
-              }
-              </Card>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Container>
+            {this.state.task.shortname === "Hate Speech" ?
+              <p className="mt-3 p-3 light-red-bg rounded white-color"><strong>WARNING</strong>: This is sensitive content! If you do not want to see any hateful examples, please switch to another task.</p>
+              : null
+            }
+            <Card className="profile-card overflow-hidden">
+              <div className="mb-1 p-3 light-gray-bg">
+                <h6 className="text-uppercase dark-blue-color spaced-header">Context:</h6>
+                {this.state.example.context && this.state.example.context.context.replace("<br>", "\n")}
+              </div>
+              <Card.Body className="overflow-auto pt-2" style={{ height: 400 }}>
+                <Card
+                  className="hypothesis rounded border m-3 card"
+                  style={{ minHeight: 120 }}
+                >
+                {this.state.example ?
+                  <>
+                  <Card.Body className="p-3">
+                    <Row>
+                      <Col xs={12} md={7}>
+                        {this.state.task.type == "extract" ?
+                          <div className="mb-3">
+                            <h6 className="text-uppercase dark-blue-color spaced-header">
+                            Question:
+                            </h6>
+                            <p>
+                            {this.state.example.text}
+                            </p>
+                            <h6 className="text-uppercase dark-blue-color spaced-header">
+                            Answer:
+                            </h6>
+                            <p>
+                            {this.state.example.target_pred}
+                            </p>
+                          </div>
+                          :
+                          <div className="mb-3">
+                            <h6 className="text-uppercase dark-blue-color spaced-header">
+                            {this.state.task.shortname === "NLI" ? "Hypothesis" : "Statement"}:
+                            </h6>
+                            <p>
+                            {this.state.example.text}
+                            </p>
+                            <h6 className="text-uppercase dark-blue-color spaced-header">
+                            Label:
+                            </h6>
+                            <p>
+                            {this.state.example.target}
+                            </p>
+                          </div>
+                        }
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                  <Card.Footer>
+                    <button
+                      data-index={this.props.index}
+                      onClick={() => this.handleResponse("correct")}
+                      type="button"
+                      className="btn btn-light btn-sm">
+                        <i className="fas fa-thumbs-up"></i> Correct
+                    </button>{" "}
+                    <button
+                      data-index={this.props.index}
+                      onClick={() => this.handleResponse("incorrect")}
+                      type="button"
+                      className="btn btn-light btn-sm">
+                        <i className="fas fa-thumbs-down"></i> Incorrect
+                    </button>{" "}
+                    <button
+                      data-index={this.props.index}
+                      onClick={() => this.handleResponse("flag")}
+                      type="button"
+                      className="btn btn-light btn-sm">
+                        <i className="fas fa-flag"></i> Flag
+                    </button>{" "}
+                    <button
+                      data-index={this.props.index}
+                      onClick={this.getNewExample}
+                      type="button"
+                      className="btn btn-light btn-sm pull-right">
+                        <i className="fas fa-undo-alt"></i> Skip and load new example
+                    </button>
+                  </Card.Footer>
+                  </>
+                  :
+                  <Card.Body className="p-3">
+                    <Row>
+                      <Col xs={12} md={7}>
+                        <p>No more examples to be verified. Please create more examples!</p>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                }
+                </Card>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Container>
+      </OverlayProvider>
     );
   }
 }
