9456f2623f1930c10bcf5ff94f11bdef24476610
Divyansh Kaushik <32644264+dkaushik96@users.noreply.github.com>
2020-10-12 17:54:54 -0400

    Fixed indentation mismatch

--- frontends/web/mturk-src/components/divyansh/qa-1/core.jsx
+++ frontends/web/mturk-src/components/divyansh/qa-1/core.jsx
@@ -89,9 +89,9 @@ class TaskOnboardingInstructions extends React.Component {
     return <>
         <br />
         <small> <strong>Onboarding:</strong> During the onboarding phase, you will be able to familiarize yourself with the annotation platform. 
-	<br /><br />(1) <strong>First,</strong> we will show you a passage and five questions and ask you to highlight the right answer — a contiguous region within the passage. 
-	<br /><br />(2) <strong>Following that,</strong> you will be presented with a passage and four answers and you will be required to write one question for each highlighted answer. 
-	<br /><br />(3) <strong>In the last step,</strong> you will be provided with a passage and you will be required to:
+        <br /><br />(1) <strong>First,</strong> we will show you a passage and five questions and ask you to highlight the right answer — a contiguous region within the passage. 
+        <br /><br />(2) <strong>Following that,</strong> you will be presented with a passage and four answers and you will be required to write one question for each highlighted answer. 
+        <br /><br />(3) <strong>In the last step,</strong> you will be provided with a passage and you will be required to:
         <ul>
         <li><strong>write a question</strong> — whose answer is contained in the passage.</li>
         <li><strong>highlight the answer</strong> — a contiguous region within the passage.</li>
@@ -100,7 +100,7 @@ class TaskOnboardingInstructions extends React.Component {
         <br /><br />
         <i>Submissions will be audited so do not try to cheat by writing incoherent questions or choosing incorrect answers.</i>
         <br /><br />
-	<strong>Workers who successfully complete onboarding will be presented with the main task.</strong> This platform is <b>not compatible</b> with tablets or mobile phones.
+        <strong>Workers who successfully complete onboarding will be presented with the main task.</strong> This platform is <b>not compatible</b> with tablets or mobile phones.
         </small>
         </>;
   }
@@ -111,12 +111,12 @@ class TaskOnboardingCompletedInstructions extends React.Component {
     return <>
         <br />
         <strong>Congrats on completing onboarding.</strong> We hope you are now familiar with the platform. Before you click on Complete Onboarding, here is some housekeeping.
-	<ul>
-	<li><strong>The instructions for the downstream task are different than the onboarding.</strong></li>
-	<li>Please email us at NoahTurkProject.1041@gmail.com if you do not understand any parts of the instructions or if there is anything else that we can help with. We will try to respond as quickly as possible.</li>
-	<li>If upon clicking Complete Onboarding, the interface does not re-route you to the main task, please refresh your page.</li>
-	</ul>
-	We appreciate your work and are eagerly looking forward to outputs from this HIT.
+        <ul>
+        <li><strong>The instructions for the downstream task are different than the onboarding.</strong></li>
+        <li>Please email us at NoahTurkProject.1041@gmail.com if you do not understand any parts of the instructions or if there is anything else that we can help with. We will try to respond as quickly as possible.</li>
+        <li>If upon clicking Complete Onboarding, the interface does not re-route you to the main task, please refresh your page.</li>
+        </ul>
+        We appreciate your work and are eagerly looking forward to outputs from this HIT.
         </>;
   }
 }
@@ -132,7 +132,7 @@ class TaskNoModelInstructions extends React.Component {
         </ul>
         Additionally, please follow the following guidelines: (i) ensure that the question is coherent; (ii) that the answer is unambiguous — any competent reader shown the same question and passage should select the same (or highly overlapping) answer.
         <br />After entering your question and selecting the answer, press “Submit”. You are required to follow this process 5 times for each passage <b>(remember that each question is standalone)</b>. Once you've submitted all 5 question-answer pairs, Submit HIT button will appear. Try to write questions that do not highly overlap with passage text.</small>
-	<br /><br />
+        <br /><br />
         <small style={{ color: 'red' }}><i>Submissions will be audited for quality so do not try to write incoherent questions or choose incorrect answers. </i></small><small><b>Please email us at NoahTurkProject.1041@gmail.com if you do not understand any parts of the instructions or if there is anything else that we can help with. We will try to respond as quickly as possible.</b></small>
         </>;
   }
@@ -142,7 +142,7 @@ class TaskModelInstructions extends React.Component {
   render() {
     return <>
         <br />
-	<small>
+        <small>
         <strong>Instructions:</strong> Fool the machine! In this task, you are provided with a passage (in grey). In the text field below the passage, please:
         <ul>
         <li><strong>write a question</strong> — whose answer is contained in the passage.</li>
@@ -151,7 +151,7 @@ class TaskModelInstructions extends React.Component {
         Be sure to  (i) ensure that the question is coherent; (ii) that the answer is unambiguous — any competent reader shown the same question and passage should select the same (or highly overlapping) answer.
         <br />
         After entering your question and selecting the answer, press “Submit”. The app will then highlight the AI’s predicted answer. If the AI got it wrong, then you fooled the machine! 
-	<br />You are required to follow the above process 5 times for each passage <b>(remember that each question is standalone)</b>. Once you've submitted all 5 question-answer pairs, Submit HIT button will appear. You will be provided a bonus of 15 cents for every question that fools the machine! Try to write questions that do not highly overlap with passage text.
+        <br />You are required to follow the above process 5 times for each passage <b>(remember that each question is standalone)</b>. Once you've submitted all 5 question-answer pairs, Submit HIT button will appear. You will be provided a bonus of 15 cents for every question that fools the machine! Try to write questions that do not highly overlap with passage text.
         <br /><br /></small>
         <small style={{ color: 'red' }}><i>Submissions will be audited for quality so do not try to write incoherent questions or choose incorrect answers. </i></small><small><b>Please email us at NoahTurkProject.1041@gmail.com if you do not understand any parts of the instructions or if there is anything else that we can help with. We will try to respond as quickly as possible.</b>
     </small></>;
@@ -179,7 +179,7 @@ class DivyanshQATaskMain extends React.Component {
           {this.state.showInstructions && <Row> <TaskModelInstructions /> </Row>}
           </Container>
           <CreateInterface api={this.api} {...this.props} />
-	</>;
+        </>;
     }
     else {
       return <>
@@ -190,7 +190,7 @@ class DivyanshQATaskMain extends React.Component {
           {this.state.showInstructions && <Row> <TaskNoModelInstructions /> </Row>}
           </Container>
           <CreateInterfaceNoModel api={this.api} {...this.props} />
-	</>;
+        </>;
     }
   }
 }
