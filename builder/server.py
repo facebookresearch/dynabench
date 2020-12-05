807d38f2486306d2b8b24713423989a94999884a
Douwe Kiela <douwe.kiela@cl.cam.ac.uk>
2020-12-05 05:06:07 +0000

    Example builder SQS client and server

---
+++ builder/server.py
@@ -0,0 +1,23 @@
+import sys
+import time
+
+import boto3
+
+sys.path.append("../api")
+from common.config import config
+
+if __name__ == "__main__":
+    sqs_service = boto3.client(
+        "sqs",
+        aws_access_key_id=config["aws_access_key_id"],
+        aws_secret_access_key=config["aws_secret_access_key"],
+        region_name=config["aws_region"],
+    )
+    sqs = boto3.resource("sqs")
+    queue = sqs.get_queue_by_name(QueueName="dynabench-build")
+    while True:
+        for message in queue.receive_messages():
+            body = message.body
+            print(body)
+            message.delete()
+        time.sleep(1)
