c713e8d7e4845a49b963e21ed9533da272b745fc
Jose Alberto Lopez Magana <albertolm.9115@gmail.com>
2021-01-13 00:01:19 -0600

    fixed typo in feeder and corrected image id

--- api/tools/populate_vqa.py
+++ api/tools/populate_vqa.py
@@ -8,8 +8,9 @@ import sys
 # This was needed to import the models package.
 sys.path.append("..")
 
-
 baseUrl = "http://images.cocodataset.org/annotations"
+
+imageUrl = "https://s3.us-east-1.amazonaws.com/images.cocodataset.org/{}/{}.jpg"
 
 datasets = {
     "annotations_trainval2014": [
@@ -21,25 +22,11 @@ datasets = {
 }
 
 
-def getSetName(fileName):
-    if fileName.find("_") == -1:
-        raise Exception("Invalid input.")
-    return fileName.split("_")[-1]
-
-
 def getImagesFromFile(fileName):
     path = f"annotations/{fileName}.json"
-    if not os.path.exists(path):
-        raise Exception(f"Invalid path. File {fileName} does not exist.")
     with open(path) as jsonFile:
         anns = json.load(jsonFile)
         return anns["images"]
-
-
-def getImageUrl(setName, id):
-    return (
-        f"https://s3.us-east-1.amazonaws.com/images.cocodataset.org/{setName}/{id}.jpg"
-    )
 
 
 def main():
@@ -65,37 +52,36 @@ def main():
 
         for f in datasets[ds]:
 
-            setName = getSetName(f)
+            setName = f.split("_")[-1]
             images = getImagesFromFile(f)
 
             data = []
             for image in images:
+                id = image["file_name"].split("_")[-1].split(".")[0]
                 data.append(
                     {
-                        "context": getImageUrl(setName, image["id"]),
+                        "context": imageUrl.format(setName, id),
                         "metadata_json": json.dumps(
                             {
-                                "id": image["id"],
+                                "id": id,
                                 "file_name": image["file_name"],
-                                "height": image["height"],
-                                "width": image["width"],
                                 "date_captured": image["date_captured"],
                             }
                         ),
                     }
                 )
 
-            # for context in data:
-            print(setName)
-            context = data[0]
-            print(context)
-            url = context["context"]
-            md = context["metadata_json"]
-            c = Context(round=round, context=url, metadata_json=md, tag=setName)
-            dbs.add(c)
-            dbs.flush()
+            for context in data:
+                print(setName)
+                context = data[0]
+                print(context)
+                url = context["context"]
+                md = context["metadata_json"]
+                c = Context(round=round, context=url, metadata_json=md, tag=setName)
+                dbs.add(c)
+                dbs.flush()
 
-            dbs.commit()
+                dbs.commit()
 
 
 if __name__ == "__main__":
