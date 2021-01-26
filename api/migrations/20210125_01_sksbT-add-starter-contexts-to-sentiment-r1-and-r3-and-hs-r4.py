d5658361e85afca800dffd36c67909bec5839913
Tristan Thrush <tristant@mit.edu>
2021-01-26 10:41:37 -1000

    added empty contexts to the recently added rounds

--- api/migrations/20210125_01_sksbT-add-starter-contexts-to-sentiment-r1-and-r3-and-hs-r4.py
+++ api/migrations/20210125_01_sksbT-add-starter-contexts-to-sentiment-r1-and-r3-and-hs-r4.py
@@ -12,7 +12,15 @@ __depends__ = {"20210119_01_ATDVS-add-sentiment-r3-and-rearrange"}
 steps = [
     step(
         "INSERT INTO contexts (`r_realid`, `context`) VALUES"
-        + ' (17, ""), (18, ""), (19, "")',
-        "DELETE FROM contexts WHERE r_realid IN (17, 18, 19)",
+        + " ((SELECT id FROM rounds WHERE tid=3 and rid=1),"
+        + ' "Please pretend you are reviewing a restaurant, movie, or book."),'
+        + " ((SELECT id FROM rounds WHERE tid=3 and rid=3),"
+        + ' "Please pretend you are reviewing a restaurant, movie, or book."),'
+        + " ((SELECT id FROM rounds WHERE tid=5 and rid=4),"
+        + ' "Please provide a statement below.")',
+        "DELETE FROM contexts WHERE r_realid IN"
+        + " ((SELECT id FROM rounds WHERE tid=3 and rid=1),"
+        + " (SELECT id FROM rounds WHERE tid=3 and rid=3),"
+        + " (SELECT id FROM rounds WHERE tid=5 and rid=4))",
     )
 ]
