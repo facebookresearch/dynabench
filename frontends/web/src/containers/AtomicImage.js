94c2bd1b861fe0ea27bf1fbff1d0b9afb2607ae4
Alberto López <albertolm.9115@gmail.com>
2021-02-05 13:51:15 -0600

    Added check vqa model's answer UI for the web interface (#245)

--- frontends/web/src/containers/AtomicImage.js
+++ frontends/web/src/containers/AtomicImage.js
@@ -8,26 +8,51 @@ import React from "react";
 
 class AtomicImage extends React.Component {
 
-     onImgLoad = ({target:img}) => {
-         const fixedWidth = 500
-         const ratio =  img.naturalHeight / img.naturalWidth
-         img.width = fixedWidth
-         img.height = ratio * fixedWidth
-      }
+   constructor(props) {
+      super(props);
+      this.state = { isImgLoaded: false };
+   }
 
-     render(){
-         const {src} = this.props
-         if(src && src.length > 0){
-            return (
-               <img onLoad={this.onImgLoad} src={src} style={{ alignSelf: 'center', marginTop: '20px' } } />
-            );
-         }
+   componentDidUpdate(prevProps) {
+      if (prevProps.src !== this.props.src) {
+         this.setState({ isImgLoaded: false });
+      }
+   }
+
+   render() {
+      const { src, maxSize } = this.props;
+      if (src && src.length > 0) {
+         return (
+            <>
+               <img
+                  onLoad={() => this.setState({ isImgLoaded: true })}
+                  src={src}
+                  style={{
+                     marginTop: "20px",
+                     display: this.state.isImgLoaded ? "block" : "none",
+                     alignSelf: "center",
+                     maxHeight: maxSize,
+                     maxWidth: maxSize
+                  }
+               } />
+               {!this.state.isImgLoaded &&
+                  <div className="d-flex align-items-center justify-content-center" style={{ height: 200 }}>
+                     <div className="spinner-border" role="status">
+                     <span className="sr-only">Loading...</span>
+                     </div>
+                  </div>
+               }
+            </>
+         )
+      } else {
          return (
             <div className="mb-1 p-3 light-gray-bg">
                There are no available images
             </div>
          )
-     }
-  }
+      }
+   }
+}
+
 
   export default AtomicImage;
