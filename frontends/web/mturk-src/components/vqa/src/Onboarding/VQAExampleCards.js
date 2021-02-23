abd9a203ff902dfcea2292089634755d6aa1c989
Alberto López <albertolm.9115@gmail.com>
2021-02-22 21:35:02 -0600

    Added buttons to change examples in onboarding in the main div. It also supports key shortcuts (arrows) to change example. (#297)

--- frontends/web/mturk-src/components/vqa/src/Onboarding/VQAExampleCards.js
+++ frontends/web/mturk-src/components/vqa/src/Onboarding/VQAExampleCards.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 import {
-    Card,
-    Row,
     Button,
     InputGroup,
-    Col
-} from 'react-bootstrap';
-import WarningMessage from "../WarningMessage.js"
-import AtomicImage from "../../../../../src/containers/AtomicImage.js";
+    Carousel
+} from "react-bootstrap";
+import WarningMessage from "../WarningMessage.js";
+import ExampleCard from "./ExampleCard.js";
 import { InvalidQuestionCharacteristics } from "../QuestionsCharacteristics.js";
 import getVQAValidInvalidExamples from "./Examples/VQAValidInvalidExamples.js";
 
@@ -22,26 +20,15 @@ class VQAExampleCards extends React.Component {
         }
     }
 
-    getNextExample = () => {
-
-        if (this.state.currIdx < this.examples.length - 1) {
-            this.setState({
-                currIdx: this.state.currIdx + 1,
-            }, () => {
-                if (this.state.currIdx >= this.examples.length - 1) {
-                    this.props.setPhaseCompleted();
-                }
-            });
-        }
-    }
-
-    getPrevExample = () => {
-        if (this.state.currIdx > 0) {
-            this.setState(state => {
-                return { currIdx: state.currIdx - 1 };
-            });
-        }
-    }
+    handleSelect = (selectedIndex, e) => {
+        this.setState({
+            currIdx: selectedIndex
+        }, () => {
+            if (this.state.currIdx >= this.examples.length - 1) {
+                this.props.setPhaseCompleted();
+            }
+        })
+    };
 
     render() {
         let phaseInstructionsButton = <></>;
@@ -67,21 +54,11 @@ class VQAExampleCards extends React.Component {
                 </p>
             </div>
         :   <></>
-        const exampleSelector = <InputGroup className="mb-3">
-            <Button
-                className="btn btn-sm btn-success"
-                onClick={this.getPrevExample}
-                disabled={this.state.currIdx === 0}>
-                    Previous Example
-            </Button>
-            <Button
-                className="btn btn-sm btn-success"
-                style={{ marginLeft: 5 }}
-                onClick={this.getNextExample}
-                disabled={this.state.currIdx === this.examples.length - 1}>
-                    Next Example
-            </Button>
-        </InputGroup>
+        const exampleCards = this.examples.map(example =>
+            <Carousel.Item>
+                <ExampleCard example={example} onboardingMode={this.props.onboardingMode}/>
+            </Carousel.Item>
+        );
         const exampleTracker = <small style={{ padding: 7 }}>Example: {this.state.currIdx + 1} / {this.examples.length}</small>
         return (
             <>
@@ -89,77 +66,13 @@ class VQAExampleCards extends React.Component {
                 {phaseInstructionsButton}
                 {phaseInstructions}
                 <WarningMessage/>
-                {exampleSelector}
-                <Card className="d-flex justify-content-center overflow-hidden" style={{ height: "auto" }}>
-                    <Card.Header className="mb-4">
-                        {this.examples[this.state.currIdx].isValid ? (
-                            <h5 className="text-uppercase dark-blue-color spaced-header">
-                                {this.examples[this.state.currIdx].isValid ? "Valid Question" : "Invalid Question"}
-                            </h5>
-                        ) : (
-                            <p><b style={{ color: "red" }}>INVALID QUESTION {this.props.onboardingMode === "creation" ? " - DON'T ASK THIS TYPE OF QUESTION" :  ""}</b> </p>
-                        )}
-                    </Card.Header>
-                    <AtomicImage src={this.examples[this.state.currIdx]['imageUrl']} maxHeight={400} maxWidth={600}/>
-                    <Card.Body className="overflow-auto pt-2" style={{ height: "auto" }}>
-                        <Card
-                            className="hypothesis rounded border m-3 card"
-                            style={{ minHeight: 120 }}>
-                            <Card.Body className="p-3">
-                                <Row>
-                                    <Col>
-                                        <h6 className="text-uppercase dark-blue-color spaced-header">
-                                            Question:
-                                        </h6>
-                                        <p>
-                                            <small>{this.examples[this.state.currIdx]['question']}</small>
-                                        </p>
-                                        <h6 className="text-uppercase dark-blue-color spaced-header">
-                                            Why is this {this.examples[this.state.currIdx].isValid ? (
-                                                "a valid"
-                                                ) : (
-                                                    "an invalid"
-                                                )} question?
-                                        </h6>
-                                        <p>
-                                            <small>{this.examples[this.state.currIdx]['explanation']}</small>
-                                        </p>
-                                        {this.examples[this.state.currIdx].isValid && (
-                                            <>
-                                                <h6 className="text-uppercase dark-blue-color spaced-header">
-                                                    {this.props.onboardingMode === "creation" ? "AI " : ""} Answer:
-                                                </h6>
-                                                <p>
-                                                    <small>{this.examples[this.state.currIdx]['modelAns']}</small>
-                                                </p>
-                                            </>
-                                        )}
-                                        {this.examples[this.state.currIdx].isValid && (
-                                            <>
-                                                <h6 className="text-uppercase dark-blue-color spaced-header">
-                                                    Determine if {this.props.onboardingMode === "creation" ? "AI" : "the answer"} is correct or not:
-                                                </h6>
-                                                <p>
-                                                    <small>{this.examples[this.state.currIdx]['userFeedback'][0]}</small>
-                                                </p>
-                                                {this.examples[this.state.currIdx]['userFeedback'][0] === "Incorrect" && this.props.onboardingMode === "creation" && (
-                                                    <>
-                                                        <h6 className="text-uppercase dark-blue-color spaced-header">
-                                                            Provide the correct answer:
-                                                        </h6>
-                                                        <p>
-                                                            <small>{this.examples[this.state.currIdx]['userFeedback'][1]}</small>
-                                                        </p>
-                                                    </>
-                                                )}
-                                            </>
-                                        )}
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                        </Card>
-                    </Card.Body>
-                </Card>
+                <Carousel
+                    activeIndex={this.state.currIdx}
+                    interval={null}
+                    wrap={false}
+                    onSelect={this.handleSelect}>
+                    {exampleCards}
+                </Carousel>
                 {exampleTracker}
             </>
         )
