0a102a6578d632fdeb7ad9066efe601315481ed7
Zhiyi Ma <mzy0369@gmail.com>
2021-04-22 17:16:02 +0100

    Prefix predictions tempfile with job names and explicitly close the low level os.open process (#414)
    * Prefix predictions tempfile with job names to avoid tempfile clash
    
    * Read file

--- evaluation/utils/computer.py
+++ evaluation/utils/computer.py
@@ -66,9 +66,8 @@ class MetricsComputer:
                 job.endpoint_name, raw=True, perturb_prefix=perturb_prefix
             )
             raw_s3_bucket, raw_s3_path = parse_s3_uri(raw_output_s3_uri)
-
             # download raw predictions and parse
-            raw_pred_file = tempfile.mkstemp(prefix="predictions")[1]
+            fd, raw_pred_file = tempfile.mkstemp(suffix="raw", prefix=job.job_name)
             self.s3_client.download_file(raw_s3_bucket, raw_s3_path, raw_pred_file)
             with open(raw_pred_file) as f:
                 tmp = ""
@@ -88,6 +87,7 @@ class MetricsComputer:
                     elif line:
                         tmp += line.replace("\n", "")
                     line = f.readline().strip()
+            os.close(fd)
             os.remove(raw_pred_file)
 
             # upload parsed file
@@ -95,14 +95,19 @@ class MetricsComputer:
                 job.endpoint_name, raw=False, perturb_prefix=perturb_prefix
             )
             s3_bucket, s3_path = parse_s3_uri(output_s3_uri)
-            parsed_pred_file = tempfile.mkstemp(prefix="predictions")[1]
+            fd, parsed_pred_file = tempfile.mkstemp(
+                suffix="parsed", prefix=job.job_name
+            )
             with open(parsed_pred_file, "w") as f:
                 for pred in predictions:
                     f.write(json.dumps(pred) + "\n")
             self.s3_client.upload_file(parsed_pred_file, s3_bucket, s3_path)
+            os.close(fd)
             os.remove(parsed_pred_file)
         except Exception as e:
-            logger.exception(f"Exception in parsing output file for {job.name}: {e}")
+            logger.exception(
+                f"Exception in parsing output file for {job.job_name}: {e}"
+            )
             return False
         return predictions
 
