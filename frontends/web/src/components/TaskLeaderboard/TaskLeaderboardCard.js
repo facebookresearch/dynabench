70a976fff64c8559c9e333c1142444953f87f8e3
Tristan Thrush <tristant@mit.edu>
2021-05-10 18:56:39 -0700

    added help modal

--- frontends/web/src/components/TaskLeaderboard/TaskLeaderboardCard.js
+++ frontends/web/src/components/TaskLeaderboard/TaskLeaderboardCard.js
@@ -6,6 +6,7 @@ import {
   Button,
   Tooltip,
   OverlayTrigger,
+  Modal,
 } from "react-bootstrap";
 import UserContext from "../../containers/UserContext";
 import OverallModelLeaderBoard from "./OverallModelLeaderBoard";
@@ -30,6 +31,7 @@ const TaskLeaderboardCard = (props) => {
   const task = props.task;
 
   const [data, setData] = useState([]);
+  const [enableHelp, setEnableHelp] = useState(false);
   const [enableWeights, setEnableWeights] = useState(false);
   const [enableDatasetWeights, setEnableDatasetWeights] = useState(false);
   // Map task metrics to include weights for UI
@@ -189,6 +191,89 @@ const TaskLeaderboardCard = (props) => {
             : "Round " + props.displayRoundId + " Model Leaderboard"}
         </h2>
         <div className="d-flex justify-content-end flex-fill">
+          <Modal
+            size="lg"
+            show={enableHelp}
+            onHide={() => setEnableHelp(!enableHelp)}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Dynaboard Information</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              This is a dynamic leaderboard. It allows you, the user, to specify
+              your own utility function over a variety of metrics and datasets,
+              which determines the final ranking of models for this particular
+              task. The default initial weights are specified by the task
+              owners.
+              <br />
+              <br />
+              There are a few important caveats that you should keep in mind
+              when interacting with this leaderboard:
+              <br />
+              <br />
+              <ul>
+                <li>
+                  The dynascore is dynamic. It is computed with respect to other
+                  models in the system, and thus keeps changing over time. We do
+                  not recommend reporting solely on the dynascore in papers,
+                  unless it is clearly accompanied with a reference to the
+                  dynamic leaderboard.
+                </li>
+                <li>
+                  The fairness and robustness metrics are far from perfect.
+                  There is no singular way to measure model fairness. Whether to
+                  include any explicit fairness metric was a difficult choice –
+                  we acknowledge that we might inadvertently facilitate false or
+                  spurious fairness claims, fairness-value hacking, or give
+                  people a false sense of fairness. The research community has a
+                  long way to go in terms of developing well-defined concrete
+                  fairness measurements. Ultimately, however, we came to the
+                  conclusion that fairness is such an important axis of
+                  evaluation that we would rather have an imperfect metric than
+                  no metric at all: in our view, a multi-metric model evaluation
+                  framework simply must include fairness as a primary evaluation
+                  axis. Please refer to the paper for more details. We encourage
+                  the community to come up with better blackbox fairness and
+                  robustness metrics.
+                </li>
+                <li>
+                  The compute and memory metrics are computed using AWS
+                  Cloudwatch. Throughput in examples per second is computed as
+                  the total number of examples divided by the inference time in
+                  seconds. The inference time is the difference between
+                  TransformEndTime and TransformStartTime from AWS’s
+                  DescribeTransformJob API. Memory is the average of all logged
+                  MemoryUtilization data points (logged as a utilization
+                  percentage every 1 minute by AWS) during inference, which is
+                  converted into GiB by multiplying with the total available
+                  memory of the instance type. Note that this is the memory
+                  utilization of the entire model’s docker container that serves
+                  the model. Both metrics are dependent on the instance type,
+                  and contain some randomness, i.e. that they are expected to
+                  change slightly every time even in exactly the same setup. In
+                  our setup, ml.m5.2xlarge is the default machine instance,
+                  which has 8 cpus and 32 GiB memory. All metrics are
+                  higher-is-better, except memory, where lower is better.
+                </li>
+              </ul>
+              For more details, see the paper.
+            </Modal.Body>
+          </Modal>
+          <OverlayTrigger
+            placement="top"
+            overlay={<Tooltip id="tip-metric-weights">Help</Tooltip>}
+          >
+            <Button
+              className="btn bg-transparent border-0"
+              onClick={() => {
+                setEnableHelp(!enableHelp);
+              }}
+            >
+              <span className="text-black-50">
+                <i className="fas fa-question"></i>
+              </span>
+            </Button>
+          </OverlayTrigger>
           <OverlayTrigger
             placement="top"
             overlay={<Tooltip id="tip-metric-weights">Metric Weights</Tooltip>}
