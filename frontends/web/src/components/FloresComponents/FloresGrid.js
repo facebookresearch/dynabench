9cfc40dd33e8ae61f25d60e9804f30cdd8a1844c
Sahir Gomez <sahir@fb.com>
2021-05-25 14:55:00 -0400

    FloRes task and model page update

--- frontends/web/src/components/FloresComponents/FloresGrid.js
+++ frontends/web/src/components/FloresComponents/FloresGrid.js
@@ -1,11 +1,153 @@
-import React from 'react'
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-const FloresGrid = () => {
-    return (
-        <div>
-            FloRes Grid .. In progress
-        </div>
-    )
-}
+import React from "react";
+import Highcharts from "highcharts";
+import highchartsHeatmap from "highcharts/modules/heatmap";
+import HighchartsReact from "highcharts-react-official";
+import highchartsBoost from "highcharts/modules/boost";
+
+import "./FloresGrid.css";
+
+highchartsHeatmap(Highcharts);
+highchartsBoost(Highcharts);
+
+const FloresGrid = (props) => {
+  const getPointCategoryName = (point, dimension) => {
+    var series = point.series,
+      isY = dimension === "y",
+      axis = series[isY ? "yAxis" : "xAxis"];
+    return axis.categories[point[isY ? "y" : "x"]];
+  };
+
+  const chartOptions = {
+    marginTop: 0,
+    marginBottom: 0,
+    credits: {
+      enabled: false,
+    },
+
+    chart: {
+      type: "heatmap",
+      height: 720,
+      width: 720,
+      showAxes: true,
+      //stacking: "overlap",
+      spacing: [0, 0, 0, 0],
+      //borderWidth: 1,
+    },
+
+    colorAxis: {
+      min: 0,
+      minColor: "#FFFFFF",
+      maxColor: Highcharts.getOptions().colors[0],
+    },
+
+    xAxis: {
+      categories: ["en_XX", "ru_RU", "ba_BA", "ar_AR", "al_AL", "hu_HU"],
+    },
+
+    yAxis: {
+      categories: ["en_XX", "ru_RU", "ba_BA", "ar_AR", "al_AL", "hu_HU"],
+      title: null,
+      reversed: true,
+    },
+
+    title: {
+      text: props.model.name || "Unknown",
+    },
+
+    legend: {
+      enabled: false,
+    },
+
+    plotOptions: {
+      heatmap: {
+        color: "grey",
+        crisp: false,
+      },
+    },
+
+    tooltip: {
+      formatter() {
+        let self = this;
+        console.log(self);
+        const valuePoint = self.point.value;
+
+        return (
+          "<br> -------  ------- <br>" +
+          "<b>" +
+          getPointCategoryName(this.point, "y") +
+          "-" +
+          getPointCategoryName(this.point, "x") +
+          ": " +
+          "</b>" +
+          valuePoint +
+          "<br> -------  ------- <br>"
+        );
+      },
+    },
+    series: [
+      {
+        name: "Tags",
+        borderWidth: 0.5,
+        data: [
+          [0, 0, ""],
+          [0, 1, 34],
+          [0, 2, 28],
+          [0, 3, 24],
+          [0, 4, 67],
+          [0, 5, 47],
+          [1, 0, 92],
+          [1, 1, ""],
+          [1, 2, 78],
+          [1, 3, 90],
+          [1, 4, 48],
+          [1, 4, 38],
+          [1, 5, 58],
+          [2, 0, 35],
+          [2, 1, 15],
+          [2, 2, ""],
+          [2, 3, 64],
+          [2, 4, 52],
+          [2, 5, 65],
+          [3, 0, 72],
+          [3, 1, 42],
+          [3, 2, 84],
+          [3, 3, ""],
+          [3, 4, 16],
+          [3, 5, 66],
+          [4, 0, 38],
+          [4, 1, 5],
+          [4, 2, 8],
+          [4, 3, 67],
+          [4, 4, ""],
+          [4, 5, 78],
+          [5, 0, 72],
+          [5, 1, 42],
+          [5, 2, 84],
+          [5, 3, 32],
+          [5, 4, 16],
+          [5, 5, ""],
+        ],
+        dataLabels: {
+          enabled: true,
+          color: "#000000",
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="scroll-wrapper">
+      <div className="mr-auto ml-auto mt-4">
+        <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+      </div>
+    </div>
+  );
+};
 
 export default FloresGrid;
--- frontends/web/src/containers/FloresModelPage.js
+++ frontends/web/src/containers/FloresModelPage.js
@@ -13,7 +13,7 @@ import {
   Card,
   Button,
   Table,
-  Spinner
+  Spinner,
 } from "react-bootstrap";
 import Moment from "react-moment";
 import Markdown from "react-markdown";
@@ -22,186 +22,181 @@ import TasksContext from "./TasksContext";
 import UserContext from "./UserContext";
 import FloresGrid from "../components/FloresComponents/FloresGrid";
 
-
 const FloresModelPage = (props) => {
+  const context = useContext(UserContext);
+  const { modelId } = props.match.params;
 
-    const context = useContext(UserContext);
-    const { modelId } = props.match.params;
+  const [isLoading, setIsLoading] = useState(false);
+  const [ctxUserId, setCtxUserId] = useState(null);
+  const [model, setModel] = useState({});
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [ctxUserId, setCtxUserId] = useState(null)
-    const [model, setModel] = useState({});
-
-    // Call api to fetch model
-    useEffect(() => {
-      /**
-       * Invoke APIService to fetch leader board data
-       *
-       * @param {*} api instance of @see APIService
-       * @param {number} page
-       */
-      const fetchFloresModel = (api) => {
-          api.getModel(108).then(
-            (result) => {
-              setModel(result)
-            },
-            (error) => {
-              console.log(error);
-              if (error.status_code === 404 || error.status_code === 405) {
-                props.history.push("/");
-              }
-            }
-          );
-            
-      };
-  
-      setIsLoading(true);
-  
-      fetchFloresModel(context.api);
-      setIsLoading(false);
-      return () => {};
-    }, [modelId]);
-
-    useEffect(() =>{
-      const user = context.api.getCredentials();
-      setCtxUserId( user.id );
-    }, [])
-
-    console.log(model)
-
-    const handleBack = () => {
-      const propState = props.location.state;
-      if (propState && propState.src === "publish") {
-        props.history.push("/account#models");
-      } else {
-        props.history.goBack();
-      }
+  // Call api to fetch model
+  useEffect(() => {
+    /**
+     * Invoke APIService to fetch leader board data
+     *
+     * @param {*} api instance of @see APIService
+     * @param {number} page
+     */
+    const fetchFloresModel = (api) => {
+      api.getModel(108).then(
+        (result) => {
+          setModel(result);
+        },
+        (error) => {
+          console.log(error);
+          if (error.status_code === 404 || error.status_code === 405) {
+            props.history.push("/");
+          }
+        }
+      );
     };
 
-    if (isLoading) return <Spinner animation="border"/>
+    setIsLoading(true);
 
-    return (
-      <Container className="mb-3 pb-5 mt-4">
-        <Row>
-          <Col className="m-auto" lg={8}>
-            <Card className="my-4">
-              <Card.Header className="light-gray-bg">
-                <div className="d-flex justify-content-between">
-                  <Button
-                    className={`blue-bg border-0 font-weight-bold`}
-                    //aria-label="Back"
-                    size="sm"
-                    onClick={handleBack}
-                  >
-                     <i className="fas fa-arrow-left"></i>
-                  </Button>
-                  <div className="align-items">
-                    <h2 className="align-items text-uppercase m-0 text-reset">
-                      Model Overview
-                    </h2>
-                  </div>
-                 
-                </div>
-               
-              </Card.Header>
-              <Card.Body className="p-0 leaderboard-container">
-                <div className="mx-2 mt-4 p-0">
-                  <p><span className="font-weight-bold">Model Name - </span> {model.name || "Unknown"}</p>
-                  <p className="upload-time">
-                    Uploaded{" "}
-                    <Moment utc fromNow>
-                      {model.upload_datetime}
-                    </Moment>
-                  </p>
-                </div>
-                <Table hover className="mb-0 floresModel">
-                  <thead />
-                  <tbody>
-                    <tr style={{ border: `none` }}>
-                      <td>Owner</td>
-                      <td>
-                        <Link to={`/users/${model.user_id}`}>
-                          {model.username}
-                        </Link>
-                      </td>
-                    </tr>
-                    <tr style={{ border: `none` }}>
-                      <td>Task</td>
-                      <td>
-                        <Link to={`/tasks/${model.tid}#overall`}>
-                          <TasksContext.Consumer>
-                            {({ tasks }) => {
-                              const task =
-                                model &&
-                                tasks.filter((e) => e.id === model.tid);
-                              return (
-                                task && task.length && task[0].shortname
-                              );
-                            }}
-                          </TasksContext.Consumer>
-                        </Link>
-                      </td>
-                    </tr>
-                    <tr style={{ border: `none` }}>
-                      <td>Summary</td>
-                      <td>{model.longdesc}</td>
-                    </tr>
-                    <tr style={{ border: `none` }}>
-                      <td style={{ whiteSpace: "nowrap" }}># Parameters</td>
-                      <td>{model.params}</td>
-                    </tr>
-                    <tr style={{ border: `none` }}>
-                      <td>Language(s)</td>
-                      <td>{model.languages}</td>
-                    </tr>
-                    <tr style={{ border: `none` }}>
-                      <td>License(s)</td>
-                      <td>{model.license}</td>
-                    </tr>
-                    <tr style={{ border: `none` }}>
-                      <td style={{ verticalAlign: "middle" }}>
-                        Model Card
-                      </td>
-                      <td className="modelCard">
-                        <Markdown>{"..."}</Markdown>
-                      </td>
-                    </tr>          
-                  </tbody>
-                </Table>
-              </Card.Body>    
-            </Card>
-            
-          </Col>
-        </Row>
+    fetchFloresModel(context.api);
+    setIsLoading(false);
+    return () => {};
+  }, [context.api, modelId, props.history]);
 
-        <Row>
-          <Col className="m-auto" lg={12}>
-            <Card className="my-4">
-              <Card.Header className="light-gray-bg">
-                <div className="d-flex justify-content-between">
-                  <Button
-                    className={`blue-bg border-0 font-weight-bold`}
-                    size="sm"
-                    onClick={() => {}}
-                  >
-                     <i className="fas fa-info-circle"></i>
-                  </Button>
-                  <div className="align-items">
-                    <h2 className="align-items text-uppercase m-0 text-reset">
-                      Languages Grid
-                    </h2>
-                  </div>        
+  useEffect(() => {
+    const user = context.api.getCredentials();
+    setCtxUserId(user.id);
+  }, [context.api]);
+
+  console.log(model);
+
+  const handleBack = () => {
+    const propState = props.location.state;
+    if (propState && propState.src === "publish") {
+      props.history.push("/account#models");
+    } else {
+      props.history.goBack();
+    }
+  };
+
+  if (isLoading) return <Spinner animation="border" />;
+
+  return (
+    <Container className="mb-3 pb-5 mt-4">
+      <Row>
+        <Col className="m-auto" lg={8}>
+          <Card className="my-4">
+            <Card.Header className="light-gray-bg">
+              <div className="d-flex justify-content-between">
+                <Button
+                  className={`blue-bg border-0 font-weight-bold`}
+                  size="sm"
+                  onClick={handleBack}
+                >
+                  <i className="fas fa-arrow-left"></i>
+                </Button>
+                <div className="align-items">
+                  <h2 className="align-items text-uppercase m-0 text-reset">
+                    Model Overview
+                  </h2>
                 </div>
-               
-              </Card.Header>
-              <Card.Body className="p-0 my-0">
-                <FloresGrid/>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    )
-}
+              </div>
+            </Card.Header>
+            <Card.Body className="p-0 leaderboard-container">
+              <div className="mx-2 mt-4 p-0">
+                <p>
+                  <span className="font-weight-bold">Model Name - </span>{" "}
+                  {model.name || "Unknown"}
+                </p>
+                <hr />
+              </div>
+              <Table hover className="mb-0 floresModel">
+                <thead />
+                <tbody>
+                  <tr style={{ border: `none` }}>
+                    <td>Owner</td>
+                    <td>
+                      <Link to={`/users/${model.user_id}`}>
+                        {model.username}
+                      </Link>
+                    </td>
+                  </tr>
+                  <tr style={{ border: `none` }}>
+                    <td>Task</td>
+                    <td>
+                      <Link to={`/tasks/${model.tid}#overall`}>
+                        <TasksContext.Consumer>
+                          {({ tasks }) => {
+                            const task =
+                              model && tasks.filter((e) => e.id === model.tid);
+                            return task && task.length && task[0].shortname;
+                          }}
+                        </TasksContext.Consumer>
+                      </Link>
+                    </td>
+                  </tr>
+                  <tr style={{ border: `none` }}>
+                    <td>Summary</td>
+                    <td>{model.longdesc}</td>
+                  </tr>
+                  <tr style={{ border: `none` }}>
+                    <td style={{ whiteSpace: "nowrap" }}># Parameters</td>
+                    <td>{model.params}</td>
+                  </tr>
+                  <tr style={{ border: `none` }}>
+                    <td>Language(s)</td>
+                    <td>{model.languages}</td>
+                  </tr>
+                  <tr style={{ border: `none` }}>
+                    <td>License(s)</td>
+                    <td>{model.license}</td>
+                  </tr>
+                  <tr style={{ border: `none` }}>
+                    <td style={{ verticalAlign: "middle" }}>Model Card</td>
+                    <td className="modelCard">
+                      <Markdown>{"..."}</Markdown>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+              <div className="float-right mr-2">
+                <p className="upload-time">
+                  <i className="fas fa-clock"></i> Uploaded{" "}
+                  <Moment utc fromNow>
+                    {model.upload_datetime}
+                  </Moment>
+                </p>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col className="m-auto" lg={12}>
+          <Card className="my-4">
+            <Card.Header className="light-gray-bg">
+              <div className="d-flex justify-content-between">
+                <Button
+                  className={`blue-bg border-0 font-weight-bold`}
+                  size="sm"
+                  onClick={() => {}}
+                >
+                  <i className="fas fa-info-circle"></i>
+                </Button>
+                <div className="align-items">
+                  <h2 className="align-items text-uppercase m-0 text-reset">
+                    Performance Grid
+                  </h2>
+                </div>
+              </div>
+            </Card.Header>
+            <Card.Body className="p-4 my-0">
+              <div className="">
+                <FloresGrid model={model} />
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default FloresModelPage;
