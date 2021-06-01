626dfdc9ed19c367f53cd9e5110774c42311a577
Tristan Thrush <tristant@mit.edu>
2021-05-31 20:54:56 -0700

    hs r6

---
+++ api/migrations/20210531_01_uJdeR-hate-speech-round-6.py
@@ -0,0 +1,46 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+"""
+Add hate speech round 6.
+"""
+
+import secrets
+
+from yoyo import step
+
+
+secret = secrets.token_hex()
+
+__depends__ = {"20210526_01_1ApkT-larger-score-metadata-json"}
+
+steps = [
+    step(
+        "UPDATE tasks SET cur_round=6 WHERE id=5",
+        "UPDATE tasks SET cur_round=5 WHERE id=5",
+    ),
+    step(
+        "INSERT INTO rounds (`tid`, `rid`, `secret`, `url`, `desc`, `longdesc`,"
+        + "`total_fooled`, `total_collected`, `total_time_spent`, `start_datetime`,"
+        + "`end_datetime`, `total_verified_fooled`) VALUES (5,6,'"
+        + secret
+        + "',''https://fhcxpbltv0.execute-api.us-west-1.amazonaws.com/predict"
+        + "?model=ts1622482363-deberta6',NULL,'The target model is "
+        + '<a target="_blank" '
+        + 'href="https://arxiv.org/abs/2006.03654">DeBERTa</a> trained '
+        + "on the data from the previous rounds and the anonymized subset of "
+        + 'the English hate speech datasets listed on <a target="_blank"href='
+        + '"http://hatespeechdata.com/">http://hatespeechdata.com</a> used in '
+        + "the previous round.<br><br><b>Task owners</b>: <a href="
+        + '"https://www.bertievidgen.net/" target="_blank">Bertie Vidgen</a>; '
+        + '<a href="mailto:zeerak.w@gmail.com">Zeerak Waseem</a>.\',0,0,'
+        + "NULL,NULL,NULL,0)",
+        "DELETE FROM rounds WHERE tid=5 and rid=6",
+    ),
+    step(
+        "INSERT INTO contexts (`r_realid`, `context`) VALUES ("
+        + "(SELECT id from rounds where rid=6 and tid=5), "
+        + "'Please provide a statement below.')",
+        "DELETE FROM contexts WHERE r_realid=(SELECT id from rounds where "
+        + "tid=5 and rid=6)",
+    ),
+]
