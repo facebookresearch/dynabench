e83172ecae0d094444c639e8e7c62295a2d534b8
Zhiyi Ma <mazhiyi@fb.com>
2021-06-30 12:40:06 +0100

    Make task code required column and fill current null entries

---
+++ api/migrations/20210630_01_ZczVK-make-task-code-required.py
@@ -0,0 +1,43 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+"""
+Make task code required
+"""
+
+from yoyo import step
+
+
+__depends__ = {"20210623_01_nG0YL-add-new-round-for-qa-task"}
+
+
+def get_task_code(shortname):
+    return "_".join(shortname.lower().split())
+
+
+steps = [
+    # First apply customized task codes to match existing folders on S3
+    step(
+        """
+        UPDATE tasks SET task_code='dkqa' WHERE shortname='DK_QA'
+        """,
+        """
+        UPDATE tasks SET task_code=NULL WHERE shortname='DK_QA'
+        """,
+    ),
+    # for tasks without a folder on S3, or any locally created tasks, no reverse
+    step(
+        """
+        UPDATE tasks SET
+        task_code=(REPLACE(REPLACE(LOWER(shortname), " ", "_"), "-", "_"))
+        WHERE task_code IS NULL
+        """
+    ),
+    step(
+        """
+        ALTER TABLE tasks MODIFY `task_code` varchar(255) NOT NULL
+        """,
+        """
+        ALTER TABLE tasks MODIFY `task_code` varchar(255) DEFAULT NULL
+        """,
+    ),
+]
