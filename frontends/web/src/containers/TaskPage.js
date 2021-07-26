14b484d22327e921a130c97ca9e6fcb706e16358
Tristan Thrush <tristant@mit.edu>
2021-07-26 10:18:48 -0700

    linted

--- frontends/web/src/containers/TaskPage.js
+++ frontends/web/src/containers/TaskPage.js
@@ -719,17 +719,17 @@ class TaskPage extends React.Component {
                         onChange={() => {
                           this.setState(
                             {
-                              validateNonFooling: !this.state
-                                .validateNonFooling,
+                              validateNonFooling:
+                                !this.state.validateNonFooling,
                             },
                             () =>
                               this.context.api.updateTaskSettings(
                                 this.state.task.id,
                                 {
-                                  validate_non_fooling: this.state
-                                    .validateNonFooling,
-                                  num_matching_validations: this.state
-                                    .numMatchingValidations,
+                                  validate_non_fooling:
+                                    this.state.validateNonFooling,
+                                  num_matching_validations:
+                                    this.state.numMatchingValidations,
                                 }
                               )
                           );
@@ -760,10 +760,10 @@ class TaskPage extends React.Component {
                                 this.context.api.updateTaskSettings(
                                   this.state.task.id,
                                   {
-                                    validate_non_fooling: this.state
-                                      .validateNonFooling,
-                                    num_matching_validations: this.state
-                                      .numMatchingValidations,
+                                    validate_non_fooling:
+                                      this.state.validateNonFooling,
+                                    num_matching_validations:
+                                      this.state.numMatchingValidations,
                                   }
                                 )
                             );
