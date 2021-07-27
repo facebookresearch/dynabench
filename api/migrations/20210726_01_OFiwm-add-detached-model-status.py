98fd202ef30ed3d209cc30e5b63aa7e5700afd7c
Tristan Thrush <tristant@mit.edu>
2021-07-26 17:42:45 -0700

    added migration

--- api/migrations/20210726_01_OFiwm-add-detached-model-status.py
+++ api/migrations/20210726_01_OFiwm-add-detached-model-status.py
@@ -1,10 +1,17 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
 """
 Add detached model status
 """
 
 from yoyo import step
 
-__depends__ = {'20210630_01_s8Xod-update-model-url-to-authorized-endpoint', '20210706_01_a9a26-fix-flores-description', '20210714_01_Fbdh6-add_flores_unpublished_dynaboard_setting'}
+
+__depends__ = {
+    "20210630_01_s8Xod-update-model-url-to-authorized-endpoint",
+    "20210706_01_a9a26-fix-flores-description",
+    "20210714_01_Fbdh6-add_flores_unpublished_dynaboard_setting",
+}
 
 steps = [
     step(
