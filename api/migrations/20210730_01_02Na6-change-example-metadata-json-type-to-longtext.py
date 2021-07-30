35177f54e2cc63decd68776628bd55d8e6e5a1f1
Max Bartolo <maxbartolo@gmail.com>
2021-07-30 19:18:35 +0100

    Migration for converting example metadata_json type to LONGTEXT

---
+++ api/migrations/20210730_01_02Na6-change-example-metadata-json-type-to-longtext.py
@@ -0,0 +1,17 @@
+"""
+Change example metadata_json type to LONGTEXT
+"""
+
+from yoyo import step
+
+__depends__ = {
+    '20210630_01_s8Xod-update-model-url-to-authorized-endpoint', 
+    '20210706_01_a9a26-fix-flores-description', 
+    '20210714_01_Fbdh6-add_flores_unpublished_dynaboard_setting'
+}
+
+steps = [
+    step("ALTER TABLE examples MODIFY metadata_json LONGTEXT"),
+    step("ALTER TABLE examples MODIFY metadata_json TEXT")
+]
+
