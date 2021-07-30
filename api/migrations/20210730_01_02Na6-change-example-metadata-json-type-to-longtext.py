b0bd0a0a3db3af87922de11f776b66c91ff0534e
Max Bartolo <maxbartolo@gmail.com>
2021-07-30 19:39:33 +0100

    Linting

--- api/migrations/20210730_01_02Na6-change-example-metadata-json-type-to-longtext.py
+++ api/migrations/20210730_01_02Na6-change-example-metadata-json-type-to-longtext.py
@@ -1,17 +1,19 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
 """
-Change example metadata_json type to LONGTEXT
+Change example metadata_json type from TEXT to LONGTEXT
 """
 
 from yoyo import step
 
+
 __depends__ = {
-    '20210630_01_s8Xod-update-model-url-to-authorized-endpoint', 
-    '20210706_01_a9a26-fix-flores-description', 
-    '20210714_01_Fbdh6-add_flores_unpublished_dynaboard_setting'
+    "20210630_01_s8Xod-update-model-url-to-authorized-endpoint",
+    "20210706_01_a9a26-fix-flores-description",
+    "20210714_01_Fbdh6-add_flores_unpublished_dynaboard_setting",
 }
 
 steps = [
     step("ALTER TABLE examples MODIFY metadata_json LONGTEXT"),
-    step("ALTER TABLE examples MODIFY metadata_json TEXT")
+    step("ALTER TABLE examples MODIFY metadata_json TEXT"),
 ]
-
