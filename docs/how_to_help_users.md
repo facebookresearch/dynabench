751d59f49ca0bf304e0f34dfd4109d04d43ba108
Guillaume Wenzek <guw@fb.com>
2021-08-04 20:32:21 +0200

    add instructions to manually evaluate model

--- docs/how_to_help_users.md
+++ docs/how_to_help_users.md
@@ -40,10 +40,12 @@ You can use the search bar with the job name to find the relevant job.
 Job names look like: `ts1627979249-t1-flores101-small1-test-1627982902`
 which can be deconstructed as:
 `<endpoint_name>-<model_name>-<dataset_name>-<timestamp>`
-The `<endpoint_name>-<model_name>-<dataset_name>` has a maximum length
+The `<endpoint_name>-<model_name>-<dataset_name>` part has a maximum length
 allowed and can be truncated.
 
-!!! This is about to change !!!
+!!! The naming scheme is about to change !!!
+https://github.com/facebookresearch/dynabench/issues/633
+
 
 To see the logs, first click on the job, then go to the tab "Monitoring",
 then you can access the job logs.
@@ -56,3 +58,34 @@ Python traceback will also appear here.
 
 Tips: tick "view as text" checkbox and filter by "MODEL_LOG" to remove torchserve logs.
 
+If the jobs are all green, that means that the model successfully ran.
+If the user still can't see their results, it means something is wrong with
+the `computer` and that either the metrics failed to compute, or failed to be written.
+[See below](#resend-the-job-for-evaluation)
+
+
+## Find the job ouput files
+
+From the AWS list of services chose "S3".
+We have four main buckets:
+
+- evaluation-xxx contains the datasets, and the inference results
+- sagemaker-xxx/torchserve/models/ contains the model uploaded by the users
+- xxx-us-west-1 contains data for all tasks
+- xxx-us-west-2 contains Flores related data
+
+
+## Resend the job for evaluation
+
+If the models job are green, but the results are not visible that means that
+something has gone wrong with the evaluation.
+You can use the script `eval_model.py` to re-evaluate a model.
+`python eval_model.py eval --mid  MID` will re-compute the metrics.
+You can also use `python eval_model.py eval --mid  MID --inference` that will
+redo the full inference.
+This can be useful for testing changes to the evaluation server,
+or if the model jobs failed because of an error on AWS side.
+But this assumes you have a running evaluation server.
+
+`python eval_model.py eval --mid  MID` works in autonomy and doesn't require
+an additional server.
--- evaluation/scripts/eval_model.py
+++ evaluation/scripts/eval_model.py
@@ -18,6 +18,7 @@ from datasets import load_datasets
 from eval_config import eval_config as config
 from models.dataset import DatasetModel
 from models.model import Model, ModelModel
+from models.score import Score, ScoreModel
 from models.task import Task, TaskModel
 from utils import helpers, computer
 from utils.evaluator import Job
@@ -28,25 +29,29 @@ logger = logging.getLogger("request_evaluation")
 
 MODELS = ModelModel()
 TASKS = TaskModel()
+SCORES = ScoreModel()
 
 
-def compute_metrics(model_id: int, task: Task):
+def compute_metrics(mid: int, task: Task, blocking: bool = False) -> None:
     """Compute/recompute the metric for the given model"""
-    computer = computer.MetricsComputer(config, load_datasets())
-    model_datasets = DatasetModel().getByTid(task.tid)
-
+    cmp = computer.MetricsComputer(config, load_datasets())
+    model_datasets = DatasetModel().getByTid(task.id)
+    model = get_model(mid)
     with multiprocessing.pool.Pool(4) as pool:
         for dataset in model_datasets:
-            computer.compute_one_async(pool, Job(model_id, dataset))
+            if blocking:
+                cmp.compute_one_blocking(Job(mid, dataset.name))
+            else:
+                cmp.compute_one_async(pool, Job(mid, dataset.name))
 
         pool.close()
         pool.join()
 
 
-def inference_and_eval(model_id: int, task: str, dataset: str):
-    task_config = metrics.get_task_config_safe(task)
+def inference_and_eval(mid: int, task_code: str, dataset: str) -> None:
+    task_config = metrics.get_task_config_safe(task_code)
     helpers.send_eval_request(
-        model_id=model_id,
+        model_id=mid,
         dataset_name=dataset,
         config=config,
         eval_server_id=task_config["eval_server_id"],
@@ -54,8 +59,8 @@ def inference_and_eval(model_id: int, task: str, dataset: str):
     )
 
 
-def get_model(model_id: int) -> Model:
-    return MODELS.getUnpublishedModelByMid(model_id)
+def get_model(mid: int) -> Model:
+    return MODELS.getUnpublishedModelByMid(mid)
 
 
 def get_task(model: Model) -> Task:
@@ -66,18 +71,32 @@ def get_shortname(taskname: str) -> str:
     return taskname.upper().replace("_", "-")
 
 
-def eval(model_id: int, dataset: str, inference: bool = False):
-    """Run evaluation for the given model as if it was just submitted."""
-    model = get_model(model_id)
+def eval(
+    mid: int, dataset: str = "*", inference: bool = False, blocking: bool = False
+) -> None:
+    """Run evaluation for the given model as if has just finished inference.
+
+    --mid: the model id (as seen in the urls)
+    --dataset: dataset to run the evaluation on. Defaults to all dataset of the model.
+    --inference: if set will also re-run the inference (take longer)
+    --blocking: if set will compute the metric in a blocking way
+    """
+    model = get_model(mid)
     task = get_task(model)
     if inference:
-        inference_and_eval(model_id, task.shortname, dataset)
+        inference_and_eval(mid, task.task_code, dataset)
     else:
-        compute_metrics(model_id, task.shortname)
+        compute_metrics(mid, task, blocking=blocking)
 
 
-def list_models(task: str = "", mid: int = 0):
-    """List models for the given task"""
+def list_models(task: str = "", mid: int = 0, scores: bool = False) -> None:
+    """List models for the given task or just show details about one model.
+
+    --task: the task shortname
+    --mid: alternatively just show this model details
+    --scores: if set will also show the score of the selected models
+    !!! Those scores may be confidential, some dataset are hidden from participants.
+    """
     if mid:
         models = [MODELS.getUnpublishedModelByMid(mid)]
     elif task:
@@ -91,6 +110,13 @@ def list_models(task: str = "", mid: int = 0):
 
     for model in models:
         print(model.id, model.name, model.deployment_status, model.endpoint_name)
+        if not scores:
+            continue
+        try:
+            for score in SCORES.dbs.query(Score).filter(Score.mid == model.id):
+                print(f"  - dataset {score.did} -> perf: {score.perf}")
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
