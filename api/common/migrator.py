c6cf871000ff3f8558cee435508f23773a62486a
douwekiela <douwekiela@users.noreply.github.com>
2021-08-30 14:27:15 -0700

    Add easy installation script (#667)

--- api/common/migrator.py
+++ api/common/migrator.py
@@ -22,3 +22,17 @@ def run_migrations():
         # Apply any outstanding migrations
         print(f"Running any outstanding migrations at path {migrations_path}")
         backend.apply_migrations(backend.to_apply(migrations))
+
+
+def first_time_migrations():
+    engine_url = "mysql://{}:{}@{}:3306/{}".format(
+        config["db_user"], config["db_password"], config["db_host"], config["db_name"]
+    )
+    backend = get_backend(engine_url)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    migrations_path = os.path.abspath(os.path.join(current_dir, "..", "migrations"))
+    migrations = read_migrations(migrations_path)
+
+    with backend.lock():
+        print(f"Marking all previous migrations done at path {migrations_path}")
+        backend.mark_migrations(backend.to_apply(migrations))
---
+++ api/install.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import json
+import os
+
+
+if __name__ == "__main__":
+    if not os.path.exists("common/config.py"):
+        print("Config does not exist yet, let's create it.")
+        print("NOTE: Use absolute paths where applicable!")
+
+        example_config_str = open("common/config.py.example").read()
+        config = {}
+        exec(example_config_str)
+
+        required_fields = [
+            "db_name",
+            "db_user",
+            "db_password",
+            "ssl_cert_file_path",
+            "ssl_org_pem_file_path",
+        ]
+        for field in required_fields:
+            tmp = input(f"Please enter your {field}: ")
+            config[field] = tmp
+
+        with open("common/config.py", "w") as fw:
+            fw.write("config = " + json.dumps(config, indent=4, sort_keys=True))
+            print("Wrote config to common/config.py - feel free to edit.")
+    else:
+        print("Config already exists.")
+
+    from common.migrator import first_time_migrations
+
+    first_time_migrations()
