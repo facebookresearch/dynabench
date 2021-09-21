22b5d766d6a68a3dab7544c150b739e9b4b8a43e
Tristan Thrush <tristant@mit.edu>
2021-09-20 21:56:17 -0700

    added migration to give vqa the new metric

--- api/migrations/20210916_01_Y97cI-changes_to_support_prediction_upload_and_eval.py
+++ api/migrations/20210916_01_Y97cI-changes_to_support_prediction_upload_and_eval.py
@@ -50,5 +50,87 @@ steps = [
         """UPDATE models SET evaluation_status='completed' WHERE
         deployment_status in ('created', 'deployed')"""
     ),
+    step(
+        """UPDATE tasks SET annotation_config_json='
+            {
+                "model_wrong_metric": {"type": "ask_user", "constructor_args": {}},
+                "aggregation_metric": {"type": "dynascore", "constructor_args": {}},
+                "perf_metric": {"type": "vqa_accuracy", "constructor_args":
+                    {"reference_name": "answer"}},
+                "delta_metrics": [],
+                "goal_message":
+    "enter a question and answer based on the image, such that the model is fooled.",
+                "context": [{"name": "image_url", "type": "image_url",
+                    "constructor_args": {}, "display_name": "image"}],
+                "input": [{"name": "question", "type": "string",
+                    "constructor_args": {"placeholder": "Enter question..."}}],
+                "output": [
+                    {"name": "answer", "type": "string",
+                        "constructor_args": {"placeholder": "Enter answer..."}}
+                ],
+                "metadata": {
+                    "create":
+                    [
+                        {"name": "target_answer", "type": "string",
+                            "constructor_args": {"placeholder":
+                                "The model was wrong, so enter the correct answer..."},
+                                "display_name": "answer",
+                                "model_wrong_condition": true,
+                                "display_name": "answer"}
+                    ],
+                    "validate":
+                    [
+                        {"name": "is_question_valid",
+                        "type": "multiclass",
+                        "constructor_args": {
+                            "labels": ["yes", "no"],
+                            "placeholder": "Is the question even valid?"
+                            },
+                        "validated_label_condition": "incorrect"}
+                    ]
+                }
+            }
+            ' WHERE task_code in ('vqa', 'vqa_val')""",
+        """UPDATE tasks SET annotation_config_json='
+            {
+                "model_wrong_metric": {"type": "ask_user", "constructor_args": {}},
+                "aggregation_metric": {"type": "dynascore", "constructor_args": {}},
+                "perf_metric": {"type": "squad_f1", "constructor_args":
+                    {"reference_name": "answer"}},
+                "delta_metrics": [],
+                "goal_message":
+    "enter a question and answer based on the image, such that the model is fooled.",
+                "context": [{"name": "image_url", "type": "image_url",
+                    "constructor_args": {}, "display_name": "image"}],
+                "input": [{"name": "question", "type": "string",
+                    "constructor_args": {"placeholder": "Enter question..."}}],
+                "output": [
+                    {"name": "answer", "type": "string",
+                        "constructor_args": {"placeholder": "Enter answer..."}}
+                ],
+                "metadata": {
+                    "create":
+                    [
+                        {"name": "target_answer", "type": "string",
+                            "constructor_args": {"placeholder":
+                                "The model was wrong, so enter the correct answer..."},
+                                "display_name": "answer",
+                                "model_wrong_condition": true,
+                                "display_name": "answer"}
+                    ],
+                    "validate":
+                    [
+                        {"name": "is_question_valid",
+                        "type": "multiclass",
+                        "constructor_args": {
+                            "labels": ["yes", "no"],
+                            "placeholder": "Is the question even valid?"
+                            },
+                        "validated_label_condition": "incorrect"}
+                    ]
+                }
+            }
+            ' WHERE task_code in ('vqa', 'vqa_val')""",
+    ),
     step("UPDATE tasks SET has_predictions_upload=1 WHERE task_code='vqa' limit 1"),
 ]
