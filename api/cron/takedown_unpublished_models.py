1e9879d5790af1c186cc33bde343918489db7b56
ktirumalafb <91989090+ktirumalafb@users.noreply.github.com>
2021-11-01 15:26:54 -0700

    Taking down unpublished models with `created` status  (#808)
    * Adding a `task_code` argument to delete models by a specific task code, and take down all models with `created` status (instead of just models with `deployed` status)

--- api/cron/takedown_unpublished_models.py
+++ api/cron/takedown_unpublished_models.py
@@ -6,10 +6,12 @@
 # These models can be redeployed by sending a message to the build
 # server, for example:
 # {"model_id": MODEL_ID, "s3_uri": s3_PATH_TO_SAVED_MODEL}
+import argparse
 import sys
 import traceback
 
 from models.model import DeploymentStatusEnum, ModelModel
+from models.task import TaskModel
 
 
 sys.path.remove("../evaluation")  # noqa
@@ -18,11 +20,49 @@ sys.path.append("../builder")  # noqa
 from utils.deployer import ModelDeployer  # # noqa isort:skip
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--task_code",
+        type=str,
+        default="",
+        help=(
+            "The script will take down all unpublished model endpoints for every task."
+            "Here, you can additionally specify a task where you want to take down"
+            "all published model endpoints as well."
+        ),
+    )
+    args = parser.parse_args()
+
+    return args
+
+
 def main():
+    args = parse_args()
     m = ModelModel()
-    unpublished_models = m.getByPublishStatus(publish_status=False)
-    for model in unpublished_models:
-        if model.deployment_status == DeploymentStatusEnum.deployed:
+
+    # Takedown all unpublished models
+    models_to_takedown = set(m.getByPublishStatus(publish_status=False))
+
+    if args.task_code.strip() != "":
+        ops = input(
+            f"Take down all model endpoints associated with task code {args.task_code},"
+            "including published models? [y/n]"
+        )
+        if ops.lower() not in ("y", "yes"):
+            print(f"Aborting takedown script")
+            exit(1)
+
+        tm = TaskModel()
+        task = tm.getByTaskCode(args.task_code)
+        task_code_models = set(m.getByTid(task.id))
+        models_to_takedown = models_to_takedown.union(task_code_models)
+
+    for model in models_to_takedown:
+        if (
+            model.deployment_status == DeploymentStatusEnum.deployed
+            or model.deployment_status == DeploymentStatusEnum.created
+        ):
             print(f"Removing model: {model.name} at endpoint {model.endpoint_name}")
             try:
                 deployer = ModelDeployer(model)
