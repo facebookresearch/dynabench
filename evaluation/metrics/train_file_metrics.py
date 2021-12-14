9364dcdd53e52d0f21b885c0cd8fe844a8ff5e4b
TristanThrush <tthrush@fb.com>
2021-12-14 00:38:41 -0800

    added real dataperf dataset (#858)

--- evaluation/metrics/train_file_metrics.py
+++ evaluation/metrics/train_file_metrics.py
@@ -2,41 +2,23 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import tempfile
+
+import boto3
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
 from eval_config import eval_config
-
-from .dataperf_dataloader import OpenImagesDataset
-
-
-dataperf_data_path = eval_config.get("dataperf_data_path", None)
-
-if dataperf_data_path is not None:
-    train_dataset_embeddings = OpenImagesDataset(
-        dataperf_data_path + "/saved_metadata",
-        dataperf_data_path + "/saved_embeddings/train",
-        split="train",
-    ).embeddings
-    test_dataset_embeddings = OpenImagesDataset(
-        dataperf_data_path + "/saved_metadata",
-        dataperf_data_path + "/saved_embeddings/test",
-        split="test",
-    ).embeddings
+from utils.helpers import download_s3_dir
 
 
-def get_train_dataset_embeddings(index):
-    return np.random.rand(
-        256
-    )  # TODO make this come from the actual embeddings when dataperf finalizes
-    # their dataset
-
-
-def get_test_dataset_embeddings(index):
-    return np.random.rand(
-        256
-    )  # TODO make this come from the actual embeddings when dataperf finalizes
-    # their dataset
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=eval_config["dataperf_aws_access_key_id"],
+    aws_secret_access_key=eval_config["dataperf_aws_secret_access_key"],
+    region_name=eval_config["dataperf_aws_region"],
+)
 
 
 def dataperf(train, test, constructor_args):
@@ -52,12 +34,44 @@ def dataperf(train, test, constructor_args):
 
     preds_formatted: [{"uid": test_id_0, "labels": ["Cat", "Dog", ...]}, ...]
     """
+    train_memo = {}
+
+    def get_train_dataset_embedding(uid):
+        if uid in train_memo:
+            return train_memo[uid]
+        else:
+            with tempfile.NamedTemporaryFile() as tf:
+                s3_client.download_fileobj(
+                    "dataperf-embeddings",
+                    "public_train_dataset_dynabench_formatted/train"
+                    + str(uid)
+                    + ".npy",
+                    tf,
+                )
+                array = np.load(tf.name)
+            train_memo[uid] = array
+            return array
+
+    test_memo = {}
+
+    def get_test_dataset_embedding(uid):
+        if uid in test_memo:
+            return test_memo[uid]
+        for label in constructor_args["test_labels"]:
+            with tempfile.TemporaryDirectory() as td:
+                download_s3_dir("dataperf-embeddings", label + "_test", td, s3_client)
+                df = pd.read_parquet(td, engine="pyarrow")
+                for _, row in df.iterrows():
+                    array = np.array(row["Embedding"])
+                    test_memo[row["ImageID"]] = array
+        return test_memo[uid]
+
     label_to_binary_classifiers = {}
     for label, data in train.items():
         train_X = []
         train_y = []
         for unique_id, is_present in data.items():
-            train_X.append(get_train_dataset_embeddings(int(unique_id)))
+            train_X.append(get_train_dataset_embedding(unique_id))
             train_y.append(is_present)
         differently_seeded_classifiers = []
         for i in range(constructor_args["seeds"]):
@@ -76,7 +90,7 @@ def dataperf(train, test, constructor_args):
                 differently_seeded_classifiers,
             ) in label_to_binary_classifiers.items():
                 if differently_seeded_classifiers[i].predict(
-                    [get_test_dataset_embeddings(int(obj["uid"]))]
+                    [get_test_dataset_embedding(obj["uid"])]
                 ):
                     differently_seeded_predictions[-1].append(label)
         preds_formatted.append(
