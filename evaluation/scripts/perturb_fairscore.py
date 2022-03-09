86c26a1aa4347419eaa26cf872ae5a423bd910ce
Rebecca Qian <rebeccaqian@fb.com>
2022-03-09 17:17:14 -0500

    Refactor fairscore perturb functions

--- evaluation/scripts/perturb_fairscore.py
+++ evaluation/scripts/perturb_fairscore.py
@@ -1,13 +1,52 @@
 from augly_perturbation import AuglyPerturbation
 from textflint import UTSample
 from textflint.generation.transformation.UT.prejudice import Prejudice
+from typing import Any, Dict, List, Optional, Tuple
 import argparse
 import json
 import csv
 
-def load_examples(path):
+
+def load_examples(path: str) -> List[Dict[str, Any]]:
+    """Load examples in .jsonl format
+    """
     with open(path) as f:
         return [json.loads(line) for line in f]
+
+
+def textflint_perturb_gender(examples):
+    """Run TextFlint gender swaps using the Prejudice transformation.
+
+    Store outputs as `textflint_rewrite` and update rows in place.
+    """
+    for row in examples:
+        # TextFlint requires field "x" to be set
+        row["x"] = row["original"]
+        # Initialize a Universal Transformation sample with the example
+        sample = UTSample(row)
+        # Name is the gender transform change type
+        gender_transform = Prejudice(change_type="Name")
+        transform_sample = gender_transform.transform(sample)
+        if transform_sample == []:
+            # If TextFlint did not detect any words to swap, use the original rewrite
+            row["textflint_rewrite"] = row["original"]
+        else:
+            textflint_rewrite = transform_sample[0].dump()
+            row["textflint_rewrite"] = textflint_rewrite["x"]
+
+
+def write_perturb_outputs_to_csv(annotated_examples, augly_perturbed_examples, output_file):
+    """Collate results from heuristic perturbation functions with original data and write to CSV path.
+    """
+    with open(output_file, "w") as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter="|")
+        csv_writer.writerow(["original", "text_with_params", "augly_rewrite", "textflint_rewrite", "perturber_rewrite", "annotator_rewrite"])
+        for annotated_ex, augly_output in zip(annotated_examples, augly_perturbed_examples):
+            # import ipdb; ipdb.set_trace
+            if len(augly_output) == 0:
+                csv_writer.writerow([annotated_ex["original"], annotated_ex["text_with_params"], annotated_ex["original"], annotated_ex["textflint_rewrite"], annotated_ex["perturber_rewrite"], annotated_ex["annotator_rewrite"]])
+                continue
+            csv_writer.writerow([annotated_ex["original"], annotated_ex["text_with_params"], augly_output[0]["original"], annotated_ex["textflint_rewrite"], annotated_ex["perturber_rewrite"], annotated_ex["annotator_rewrite"]])
 
 
 def main():
@@ -15,7 +54,7 @@ def main():
     parser.add_argument(
         "--local_path",
         type=str,
-        default="o",
+        required=True,
         help=(
             "Local path to a .jsonl file containing annotator and perturber rewrites"
         ),
@@ -23,36 +62,22 @@ def main():
     parser.add_argument(
         "--output_file",
         type=str,
-        default="03-05-allowlist-test-set-gender.csv",
+        required=True,
         help=(
             "Local path to a .jsonl file containing annotator and perturber rewrites"
         ),
     )
     args = parser.parse_args()
 
-    orig_examples = load_examples(args.local_path)
+    annotated_examples = load_examples(args.local_path)
 
+    # Run AugLy perturbations on original text fields
     pert = AuglyPerturbation(perturb_prefix="fairness", seed=2000, num_threads=1, perturb_fields=["original"], ignore_words_fields=[])
-    perturbed_examples = pert.perturb(orig_examples)
-    for ex in orig_examples:
-        ex["x"] = ex["original"]
-        sample = UTSample(ex)
-        trans = Prejudice(change_type="Name")
-        trans_sample = trans.transform(sample)
-        if trans_sample == []:
-            ex["textflint_rewrite"] = ex["original"]
-        else:
-            textflint_rewrite = trans_sample[0].dump()
-            ex["textflint_rewrite"] = textflint_rewrite["x"]
+    augly_perturbed_examples = pert.perturb(annotated_examples)
 
-    with open(args.output_file, "w") as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter="|")
-        csv_writer.writerow(["original", "text_with_params", "augly_rewrite", "textflint_rewrite", "perturber_rewrite", "annotator_rewrite"])
-        for orig, example in zip(orig_examples, perturbed_examples):
-            if len(example) == 0:
-                csv_writer.writerow([orig["original"], orig["text_with_params"], orig["original"], orig["textflint_rewrite"], orig["perturber_rewrite"], orig["annotator_rewrite"]])
-                continue
-            csv_writer.writerow([orig["original"], orig["text_with_params"], example[0]["original"], orig["textflint_rewrite"], orig["perturber_rewrite"], example[0]["annotator_rewrite"]])
+    textflint_perturb_gender(annotated_examples)
+
+    write_perturb_outputs_to_csv(annotated_examples, augly_perturbed_examples, args.output_file)
 
     
 if __name__ == "__main__":
